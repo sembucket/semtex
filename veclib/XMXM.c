19552d2be2d10f947f4589e7b0f4275830ea3dc1
hmb <>
1994-07-22 06:29:12 +0000

    Initial revision

---
+++ veclib/XMXM.c
@@ -0,0 +1,75 @@
+/*****************************************************************************
+ * xmxm() - Matrix multiply.                                                 *
+ *                                                                           *
+ * The following function computes the matrix product C = A * B.             *
+ * The matrices are assumed to be in row-major order and must occupy         *
+ * consecutive memory locations.  The input quantities are as follows:       *
+ *                                                                           *
+ *     mxm(A,nra,B,nca,C,ncb)                                                * 
+ *                                                                           *
+ *     A    ... double* ... source vector one                                *
+ *     nra  ... int     ... number of rows in A                              *
+ *     B    ... double* ... source operand two                               *
+ *     nca  ... int     ... number of columns in A                           *
+ *     C    ... double* ... result vector                                    *
+ *     ncb  ... int     ... number of columns in B                           *
+ *                                                                           *
+ *****************************************************************************/
+
+#include <stdio.h>
+#include <alplib.h>
+
+#if !defined(mxm) 
+
+void dmxm (double* A, int nra, double* B, int nca, double *C, int ncb)
+{
+  register double *a = A,
+                  *b = B,
+                  *c = C;
+  register double sum;
+  register i, j, k;
+
+  for (i = 0; i < nra; i++) {
+    for (j = 0; j < ncb; j++) {
+
+      b   = B + j;                  /* Next column of B    */
+      sum = 0.;                     /* Clear sum           */
+
+      for(k = 0; k < nca; k++) {    /* ------------------- */
+	sum += a[k] * (*b);         /* Inner product loop  */
+	b   += ncb;                 /* ------------------- */
+      }
+      *c++   = sum;                 /* Store and increment */
+    }
+    a += nca;                       /* Next row of A       */
+  }
+}
+
+
+
+
+
+void smxm (float* A, int nra, float* B, int nca, float *C, int ncb)
+{
+  register float *a = A,
+                 *b = B,
+                 *c = C;
+  register float sum;
+  register i, j, k;
+
+  for (i = 0; i < nra; i++) {
+    for (j = 0; j < ncb; j++) {
+
+      b   = B + j;
+      sum = 0.0F;
+
+      for(k = 0; k < nca; k++) {
+	sum += a[k] * (*b);
+	b   += ncb;
+      }
+      *c++   = sum;
+    }
+    a += nca;
+  }
+}
+#endif
---
+++ veclib/xmxv.c
@@ -0,0 +1,57 @@
+/*****************************************************************************
+ * xmxv: Matrix - Vector Multiply.                                           *
+ *                                                                           *
+ * This following function computes the matrix-vector product C = A * B.     *
+ * The matrix A is assumed to be stored in row-major order and must oc-      *
+ * cupy consecutive memory locations.                                        *
+ *                                                                           *
+ *      mxv(A,nra,B,nca,C)                                                   *
+ *                                                                           *
+ *      A   ... double* ... matrix factor (input)                            *
+ *      nra ... int     ... number of rows in A                              *
+ *      B   ... double* ... vector factor (input)                            *
+ *      nca ... int     ... number of columns in A                           *
+ *      C   ... double* ... vector product (output)                          *
+ *                                                                           *
+ * A more general matrix-vector multiply with arbitrary skips is given in    *
+ * mxva().                                                                   *
+ *****************************************************************************/
+
+#include <stdio.h>
+#include <alplib.h>
+
+#if !defined(mxv)
+
+void dmxv(const double* A, int nra, const double* B, int nca, double *C)
+{
+  register double *a = A,
+                  *c = C;
+  register double  sum;
+  register int     i, j;
+
+  for(i = 0; i < nra; i++) {
+    sum  = 0.0;
+    for (j = 0; j < nca; j++) sum += (*a++) * B[j]; 
+    *c++ = sum;
+  }
+}
+
+
+
+
+
+void smxv(const float* A, int nra, const float* B, int nca, float *C)
+{
+  register float  *a = A,
+                  *c = C;
+  register float  sum;
+  register int     i, j;
+
+  for(i = 0; i < nra; i++) {
+    sum  = 0.0F;
+    for (j = 0; j < nca; j++) sum += (*a++) * B[j]; 
+    *c++ = sum;
+  }
+}
+
+#endif
