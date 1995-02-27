3066c285aee5533c25af7c4a7a9bdee7ccaea0cd
hmb <>
1995-02-27 05:42:55 +0000

    a minor clean, nothing of substance.

--- veclib/xzero.c
+++ veclib/xzero.c
@@ -7,19 +7,18 @@
 
 void dzero(int n, double *x, int incx)
 {
-  register int   i;
+  if (incx == 1)
+    memset(x, '\0', n * sizeof (double));
 
+  else {
+    register int   i;
 
-  if (incx==1) {
-    memset(x, '\0', n*sizeof(double));
-    return;
-  }
+    x += (incx < 0) ? (-n + 1)*incx : 0;
 
-  x += (incx<0) ? (-n+1)*incx : 0;
-
-  for (i=0; i<n; i++) {
-    *x = 0.0;
-    x += incx;
+    for (i = 0; i < n; i++) {
+      *x = 0.0;
+      x += incx;
+    }
   }
 }
 
@@ -29,19 +28,18 @@ void dzero(int n, double *x, int incx)
 
 void izero(int n, int *x, int incx)
 {
-  register int   i;
+  if (incx == 1)
+    memset(x, '\0', n * sizeof (int));
 
+  else {
+    register int   i;
 
-  if (incx==1) {
-    memset(x, '\0', n*sizeof(int));
-    return;
-  }
+    x += (incx < 0) ? (-n + 1)*incx : 0;
 
-  x += (incx<0) ? (-n+1)*incx : 0;
-
-  for (i=0; i<n; i++) {
-    *x = 0;
-    x += incx;
+    for (i=0; i<n; i++) {
+      *x = 0;
+      x += incx;
+    }
   }
 }
 
@@ -51,18 +49,20 @@ void izero(int n, int *x, int incx)
 
 void szero(int n, float *x, int incx)
 {
-  register int   i;
 
 
-  if (incx==1) {
-    memset(x, '\0', n*sizeof(float));
-    return;
-  }
 
-  x += (incx<0) ? (-n+1)*incx : 0;
+  if (incx == 1)
+    memset(x, '\0', n * sizeof (float));
 
-  for (i=0; i<n; i++) {
-    *x = 0.0F;
-    x += incx;
+  else {
+    register int   i;
+
+    x += (incx < 0) ? (-n + 1)*incx : 0;
+
+    for (i = 0; i < n; i++) {
+      *x = 0.0F;
+      x += incx;
+    }
   }
 }
