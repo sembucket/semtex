cfc488fc57bc762520164358e49f185cb444a386
hmb <>
1995-06-29 03:09:37 +0000

    parser default values

--- femlib/defaults.h
+++ femlib/defaults.h
@@ -4,11 +4,10 @@
 
 /* $Id$ */
 
-#include <femtype.h>
-#include <femlib.h>
+#include <femdef.h>
 
 
-static struct {			/* Math constants */
+static struct {			/* Mathematical constants. */
   char  *name;
   double cval;
 } consts[] = {
@@ -17,42 +16,34 @@ static struct {			/* Math constants */
   "DEG"   ,  57.29577951308232087721 ,
   "TWOPI" ,   6.28318530717958647688 ,
   "PI"    ,   3.14159265358979323844 ,
-  NULL,       0.0
+  0       ,   0.0
 };
 
-static struct {			/* Default options (global flags) */
+static struct {			/* Default options (global flags). */
   char *name;
   int   oval;
 } option_init[] = {
-  "BINARY"  ,  0         ,	/* Save field-files in binary form */
-  "CORE"    ,  1         ,	/* Do matrix solves in memory      */
-  "BASIS"   ,  GLL       ,	/* GLL basis functions             */
-  "RULE"    ,  GL        ,	/* Gauss-Legendre quadrature rule  */
-  "PROBLEM" ,  POTENTIAL ,	/* Potential (flow) problem        */
-  NULL    ,  0
+  "GEOM"    ,  CART2D    ,
+  "BASIS"   ,  GLL       ,
+  "RULE"    ,  GL        ,
+  "PROBLEM" ,  HELMHOLTZ ,
+  "VERBOSE" ,  0         ,
+  0         ,  0
 };
 
-static struct {			/* Default integer parameters */
+static struct {			/* Default integer parameters. */
   char *name;
   int   ival;
 } iparam_init[] = {
-  "NDIM"     ,  2 ,
-  "NZ"       ,  1 ,
-  "NSTEPS"   ,  1 ,
-  "IOSTEP"   ,  1 ,
-  "VERBOSE"  ,  0 ,
-  NULL       ,  0
+  0 , 0
 };
 
-static struct {			/* Default double parameters */
+static struct {			/* Default floating point parameters. */
   char  *name;
   double dval;
 } dparam_init[] = {
-  "BETA"      ,    0.0,
-  "DELTAT"    ,    0.05,
-  "TOLABS"    ,    1.0e-8,
-  "TOLREL"    ,    1.0e-4,
-  "KINVIS"    ,    1.0,
-  "RHO"       ,    1.0,
-  NULL        ,    0.0
+  "LAMBDA2", 0.0 ,
+  "KINVIS" , 1.0 ,
+  "RHO"    , 1.0 ,
+  0        , 0.0
 };
