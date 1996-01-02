d41333b9ebacf9c63dd3311275884b5527f64f7b
hmb <>
1996-01-02 09:16:44 +0000

    Added verbosity tests, changed domain so files could be opened separately.

--- src/domain.C
+++ src/domain.C
@@ -10,13 +10,13 @@
 
 Domain::Domain (Mesh& M, const char* session, int np)
 // ---------------------------------------------------------------------------
-// Construct a new Domain with a single Field from a named file.
+// Construct a new Domain with a single Field, using geometry from M.
 //
 // On return, all information in Element and Boundary lists is set for u[0].
 // ---------------------------------------------------------------------------
 {
-  char routine[] = "Domain::Domain";
-  char s[StrMax];
+  char  routine[] = "Domain::Domain";
+  int   verbose   = option ("VERBOSE");
 
   domain_name = new char[StrMax];
   strcpy (domain_name, session);
@@ -36,28 +36,37 @@ Domain::Domain (Mesh& M, const char* session, int np)
   
   u[0] -> buildBoundaries (M);
 
-  // -- Input mesh topology information, do RCM renumbering.
+  // -- Generate mesh topology information, do RCM renumbering.
 
   u[0] -> connect  (M, np);
   u[0] -> renumber ();
 
-#ifdef DEBUG
-  Field::printConnect (u[0]);
-#endif
+  if (verbose == 2) {
+    message (routine, "-- GLOBAL NUMBERING FOR u[0]:", REMARK);
+    Field::printConnect (u[0]);
+  }
+}
 
-  // -- Clean up.
+
+void Domain::openFiles ()
+// ---------------------------------------------------------------------------
+//
+// ---------------------------------------------------------------------------
+{
+  char  routine[] = "Domain::openFiles";
+  char  s[StrMax];
 
   state_file.open   (strcat (strcpy (s, domain_name), ".sta"));
   history_file.open (strcat (strcpy (s, domain_name), ".his"));
-  field_file.open   (strcat (strcpy (s, domain_name), ".fld"));
+  if (option ("CHKPOINT"))
+    field_file.open (strcat (strcpy (s, domain_name), ".chk"));
+  else
+    field_file.open (strcat (strcpy (s, domain_name), ".fld"));
 
   if (!state_file)   message (routine, "can't open state file",   ERROR);
   if (!history_file) message (routine, "can't open history file", ERROR);
   if (!field_file)   message (routine, "can't open field file",   ERROR);
 }
-
-
-
 
 
 void Domain::addField (Field* F)
@@ -77,9 +86,6 @@ void Domain::addField (Field* F)
 }
 
 
-
-
-
 void Domain::restart ()
 // ---------------------------------------------------------------------------
 // If a restart file "name".rst can be found, use it to load
@@ -94,3 +100,59 @@ void Domain::restart ()
   if   (file) file >> *this;
   else        for (int i(0); i < iparam ("N_VAR"); i++) *u[i] = 0.0;
 }
+
+
+void  Domain::dump ()
+// ---------------------------------------------------------------------------
+// Check if a field-file write is required, carry out.
+// ---------------------------------------------------------------------------
+{
+  char  routine[] = "Domain::dump";
+  int   verbose   = option ("VERBOSE");
+
+  if (domain_step % iparam ("IO_FLD")) return;
+
+  if (option ("CHKPOINT")) {
+    char s[StrMax], b[StrMax], c[StrMax];
+
+    field_file.close ();
+    strcat (strcpy (s, domain_name), ".chk");
+    strcat (strcpy (b, domain_name), ".chk.bak");
+
+    sprintf (c, "mv ./%s ./%s", s, b);
+    system  (c);
+
+    field_file.open (s);
+    if (!field_file)
+      message (routine, "failed to open checkpoint file", ERROR);
+  }
+
+  if (verbose) message (routine, "writing field dump", REMARK);
+  field_file << *this;
+}
+
+
+void Domain::cleanup ()
+// ---------------------------------------------------------------------------
+// Do final output of domain variables if required, rename files if needed.
+// ---------------------------------------------------------------------------
+{
+  char  routine[] = "Domain::dump";
+  int   verbose   = option ("VERBOSE");
+
+  if (iparam ("N_STEP") % iparam ("IO_FLD")) {
+    if (verbose) message (routine, "writing field dump", REMARK);
+    field_file << *this;
+  }
+  field_file.close ();
+
+  if (option ("CHKPOINT")) {
+    char s[StrMax], b[StrMax], c[StrMax];
+
+    strcat  (strcpy (s, domain_name), ".chk");
+    strcat  (strcpy (b, domain_name), ".fld");
+    sprintf (c, "mv ./%s ./%s", s, b);
+    system  (c);
+  }
+}
+
