26c6345305124dfd9b09047d6e8b3df8b5fb6698
hmb <>
1996-01-02 09:01:24 +0000

    Added function to detect IEEE little/big-endian.

--- veclib/xbrev.c
+++ veclib/xbrev.c
@@ -1,6 +1,46 @@
 /*****************************************************************************
- *  xbrev:  byte-reversal routines.                                          *
+ * xbrev:  byte-reversal routines.
+ *
+ * $Id$
  *****************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int iformat (void)
+/* ------------------------------------------------------------------------- *
+ * Return 1 if machine floating-point format is IEEE little-endian,
+ * 0 if IEEE big-endian, exit to system if neither.
+ * ------------------------------------------------------------------------- */
+{
+  union {
+    float f;
+    int i;
+    unsigned char c[4];
+  } v;
+  union {
+    double d;
+    int i[2];
+    unsigned char c[8];
+  } u;
+  int doubleOK = 0,
+      intOK    = 0,
+      reverse  = (-1);
+  u.d = 3;
+  v.i = 3;
+  if      (u.c[0] == 64 && u.c[1] == 8 && v.c[3] == 3) reverse = 0;
+  else if (u.c[7] == 64 && u.c[6] == 8 && v.c[0] == 3) reverse = 1;
+#ifdef DEBUG
+  printf ("%s\n", reverse ? "Byte Reversal will occur" : "No Byte Reversal");
+#endif
+  if (reverse == -1) {
+    printf ("NO IEEE-format\n Exit\n");
+    exit   (1);
+  }
+
+  return (reverse);
+}
 
 
 void dbrev (int n, const double *x, int incx, double *y, int incy)
