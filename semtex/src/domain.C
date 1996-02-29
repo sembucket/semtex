63f1f018952b341df9e4f778f7afe2ee418ad454
hmb <>
1996-02-29 01:22:39 +0000

    Domain << & >> operators placed in domain.C. Also, these operators are no longer friend of Field, removing the coupling.

--- semtex/src/domain.C
+++ semtex/src/domain.C
@@ -2,7 +2,8 @@
  * domain.C:  implement Domain class.
  *****************************************************************************/
 
-static char RCSid[] = "$Id$";
+static char
+RCSid[] = "$Id$";
 
 #include "Fem.h"
 
@@ -39,27 +40,6 @@ Domain::Domain (Mesh& M, const char* session, int np)
 }
 
 
-void Domain::openFiles ()
-// ---------------------------------------------------------------------------
-//
-// ---------------------------------------------------------------------------
-{
-  char  routine[] = "Domain::openFiles";
-  char  s[StrMax];
-
-  state_file.open   (strcat (strcpy (s, domain_name), ".sta"));
-  history_file.open (strcat (strcpy (s, domain_name), ".his"));
-  if (option ("CHKPOINT"))
-    field_file.open (strcat (strcpy (s, domain_name), ".chk"));
-  else
-    field_file.open (strcat (strcpy (s, domain_name), ".fld"));
-
-  if (!state_file)   message (routine, "can't open state file",   ERROR);
-  if (!history_file) message (routine, "can't open history file", ERROR);
-  if (!field_file)   message (routine, "can't open field file",   ERROR);
-}
-
-
 void Domain::addField (Field* F)
 // ---------------------------------------------------------------------------
 // Add a new Field pointer to Domain's array.
@@ -93,57 +73,166 @@ void Domain::restart ()
 }
 
 
-void  Domain::dump ()
+void  Domain::dump (ofstream& output)
 // ---------------------------------------------------------------------------
 // Check if a field-file write is required, carry out.
 // ---------------------------------------------------------------------------
 {
+  int periodic = !(domain_step %  iparam ("IO_FLD"));
+  int final    =   domain_step == iparam ("N_STEP");
+
+  if ( ! (periodic || final) ) return;
+
   char  routine[] = "Domain::dump";
   int   verbose   = option ("VERBOSE");
+  int   chkpoint  = option ("CHKPOINT");
 
-  if (domain_step % iparam ("IO_FLD")) return;
-
-  if (option ("CHKPOINT")) {
+  if (chkpoint) {
     char s[StrMax], b[StrMax], c[StrMax];
 
-    field_file.close ();
-    strcat (strcpy (s, domain_name), ".chk");
-    strcat (strcpy (b, domain_name), ".chk.bak");
+    output.close ();
 
-    sprintf (c, "mv ./%s ./%s", s, b);
-    system  (c);
+    if (final)
+      strcat (strcpy (s, domain_name), ".fld");
+    else {
+      strcat (strcpy (s, domain_name), ".chk");
+      strcat (strcpy (b, domain_name), ".chk.bak");
+    
+      sprintf (c, "mv ./%s ./%s", s, b);
+      system  (c);
+    }
 
-    field_file.open (s);
-    if (!field_file)
-      message (routine, "failed to open checkpoint file", ERROR);
+    output.open (s);
+    if (!output) message (routine, "can't open checkpoint file", ERROR);
   }
 
   if (verbose) message (routine, ": writing field dump", REMARK);
-  field_file << *this;
+
+  output << *this;
+
 }
 
 
-void Domain::cleanup ()
+#include <time.h>
+
+
+ostream& operator << (ostream& strm, Domain& D)
 // ---------------------------------------------------------------------------
-// Do final output of domain variables if required, rename files if needed.
+// Output all Domain field variables on ostream in prism-compatible form.
+// Binary output only.
 // ---------------------------------------------------------------------------
 {
-  char  routine[] = "Domain::dump";
-  int   verbose   = option ("VERBOSE");
+  char *hdr_fmt[] = { 
+    "%-25s "    "Session\n",
+    "%-25s "    "Created\n",
+    "%-25s "    "Nr, Ns, Nz, Elements\n",
+    "%-25d "    "Step\n",
+    "%-25.6g "  "Time\n",
+    "%-25.6g "  "Time step\n",
+    "%-25.6g "  "Kinvis\n",
+    "%-25.6g "  "Beta\n",
+    "%-25s "    "Fields written\n",
+    "%-25s "    "Format\n"
+  };
 
-  if (iparam ("N_STEP") % iparam ("IO_FLD")) {
-    if (verbose) message (routine, ": writing field dump", REMARK);
-    field_file << *this;
-  }
-  field_file.close ();
+  char      routine[] = "ostream << Domain";
+  char      s1[StrMax], s2[StrMax];
+  time_t    tp   (::time (0));
 
-  if (option ("CHKPOINT")) {
-    char s[StrMax], b[StrMax], c[StrMax];
+  sprintf (s1, hdr_fmt[0], D.name());
+  strm << s1;
 
-    strcat  (strcpy (s, domain_name), ".chk");
-    strcat  (strcpy (b, domain_name), ".fld");
-    sprintf (c, "mv ./%s ./%s", s, b);
-    system  (c);
-  }
+  strftime (s2, 25, "%a %b %d %H:%M:%S %Y", localtime (&tp));
+  sprintf  (s1, hdr_fmt[1], s2);
+  strm << s1;
+
+  D.u[0] -> describe (s2);
+  sprintf (s1, hdr_fmt[2], s2);
+  strm << s1;
+
+  sprintf (s1, hdr_fmt[3], D.step());
+  strm << s1;
+
+  sprintf (s1, hdr_fmt[4], D.time());
+  strm << s1;
+
+  sprintf (s1, hdr_fmt[5], dparam ("DELTAT"));
+  strm << s1;
+
+  sprintf (s1, hdr_fmt[6], dparam ("KINVIS"));
+  strm << s1;
+
+  sprintf (s1, hdr_fmt[7], dparam ("BETA"));
+  strm << s1;
+
+  int k;
+  for (k = 0; k < D.nField(); k++) s2[k] = D.u[k] -> name ();
+  s2[k] = '\0';
+  sprintf (s1, hdr_fmt[8], s2);
+  strm << s1;
+
+  sprintf (s1, hdr_fmt[9], "binary");
+  strm << s1;
+
+  for (int n (0); n < D.nField (); n++) strm << D.u[n];
+
+  if (!strm) message (routine, "failed writing field file", ERROR);
+  strm << flush;
+
+  return strm;
 }
 
+
+istream& operator >> (istream& strm, Domain& D)
+// ---------------------------------------------------------------------------
+// Input all Domain field variables from prism-compatible istream.
+// ---------------------------------------------------------------------------
+{
+  char  routine[] = "strm>>Domain";
+  int   np, nz, nel, ntot, nfields;
+  int   npchk,  nzchk, nelchk;
+  char  s[StrMax], f[StrMax];
+
+  if (strm.getline(s, StrMax).eof()) return strm;
+
+  strm . getline (s, StrMax) . getline (s, StrMax);
+
+  D.u[0] -> describe (f);
+  istrstream (s, strlen (s)) >> np    >> np    >> nz    >> nel;
+  istrstream (f, strlen (f)) >> npchk >> npchk >> nzchk >> nelchk;
+  
+  if (np  != npchk ) message (routine, "element size mismatch",       ERROR);
+  if (nz  != nzchk ) message (routine, "number of z planes mismatch", ERROR);
+  if (nel != nelchk) message (routine, "number of elements mismatch", ERROR);
+  
+  ntot = np * np * nz * nel;
+  if (ntot != D.u[0] -> nTot ())
+    message (routine, "declared sizes mismatch",     ERROR);
+
+  strm . getline (s, StrMax) . getline(s, StrMax);
+
+  istrstream (s, strlen (s)) >> D.time ();
+  setDparam  ("t", D.time ());
+  
+  strm.getline(s,StrMax).getline(s,StrMax).getline(s,StrMax).getline(s,StrMax);
+  
+  nfields = 0;
+  while (isalpha (s[nfields])) nfields++;
+  if (nfields > D.nField ())
+    message (routine, "number of fields in file > number in domain", ERROR);
+  nfields = 0;
+  while (isalpha (s[nfields])) {
+    D.u[nfields] -> setName (s[nfields]);
+    nfields++;
+  }
+  
+  strm.getline (s, StrMax);
+  if (strstr (s, "binary")) {
+    for (int n = 0; n < nfields; n++) strm >> *D.u[n];
+    if (strm.bad ())
+      message (routine, "failed reading binary field file", ERROR);
+  } else if (strstr (s, "ASCII"))
+    message (routine, "can't read ASCII format field file", ERROR);
+    
+  return strm;
+}
