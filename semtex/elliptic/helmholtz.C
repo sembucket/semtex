62a09556a1b4b987a33b9e6cdd99736580f87ebd
hmb <>
1996-05-20 02:56:49 +0000

    Changed names of solver function calls to match current names.

--- semtex/elliptic/helmholtz.C
+++ semtex/elliptic/helmholtz.C
@@ -1,30 +1,38 @@
-/*****************************************************************************
- * elliptic.C:  routines to solve elliptic problems in one variable.
- *****************************************************************************/
+//////////////////////////////////////////////////////////////////////////////
+// helmholtz.C:  routines to solve elliptic problems in one variable.
+//////////////////////////////////////////////////////////////////////////////
 
-static char RCSid[] = "$Id$";
+static char
+RCSid[] = "$Id$";
+
 
 #include <Fem.h>
 
 
-void  Helmholtz (Domain* D, Mesh* M, char* forcing)
+void Helmholtz (Domain* D, char* forcing, const real& lambda2)
 // ---------------------------------------------------------------------------
 // Solve Helmholtz's equation
 //                                  2
 //               div grad u - LAMBDA  u = f(x,y)
 //
-// subject to BCs.  Parameter "LAMBDA2" needs to be set before entry.
+// subject to BCs.
 // ---------------------------------------------------------------------------
 {
-  Field*  Force = new Field (*D -> u[0], *M);
+  Field* Force = new Field (*D -> u[0]);
 
   if   (forcing) *Force = forcing;
   else           *Force = 0.0;
 
-  D -> u[0] -> evaluateBoundaries (0);  
-  D -> u[0] -> buildSys (dparam ("LAMBDA2"));
-  D -> u[0] -> solveSys (Force);
+  D -> u[0] -> evaluateBoundaries (0);
+  
+  if (option ("ITERATIVE")) {
+    *D -> u[0] = 0.0;
+     D -> u[0] -> solve (Force, lambda2);
+    
+  } else {
+    D -> u[0] -> assemble (lambda2);
+    D -> u[0] -> solve    (Force);
+  }
 
   D -> step ()++;
-  D -> dump ();
 }
