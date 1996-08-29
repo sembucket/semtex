61ccec69df8be97b011c9a0101e82b3123f224b6
hmb <>
1996-08-29 03:52:51 +0000

    Changed so there is only a single lookup table now.

--- femlib/defaults.h
+++ femlib/defaults.h
@@ -1,51 +1,59 @@
 /*****************************************************************************
  * DEFAULTS.H:  default parameter initializations for initial.y.
+ * All variables are now in a single table, everything is double precision.
+ *
+ * $Id$
  *****************************************************************************/
-
-/* $Id$ */
 
 #include <femdef.h>
 
-
-static struct {			/* Mathematical constants. */
-  char  *name;
+static struct {
+  char*  name;
   double cval;
 } consts[] = {
-  "PHI"   ,   1.61803398874989484820 ,
-  "E"     ,   2.71828182845904523536 ,
-  "DEG"   ,  57.29577951308232087721 ,
-  "TWOPI" ,   6.28318530717958647688 ,
-  "PI"    ,   3.14159265358979323844 ,
-  0       ,   0.0
+
+  /* -- Mathematical constants. */
+
+  "PHI"      ,   1.61803398874989484820 ,
+  "E"        ,   2.71828182845904523536 ,
+  "DEG"      ,  57.29577951308232087721 ,
+  "TWOPI"    ,   6.28318530717958647688 ,
+  "PI"       ,   3.14159265358979323844 ,
+  
+  /* -- Default named parameters. */
+
+  "TOL_REL"  ,   1.0e-6,
+  "TOL_ABS"  ,   1.0e-6,
+  "BETA"     ,   1.0   ,	/* -- TWOPI / Lz. */
+  "LAMBDA2"  ,   0.0   ,	/* -- Helmholtz constant. */
+  "KINVIS"   ,   1.0   ,
+  "RHO"      ,   1.0   ,
+
+  /* -- Option switches. */
+
+  "ITERATIVE",   0        ,
+  "PROBLEM"  ,   HELMHOLTZ,
+  "BINARY"   ,   0        ,
+  "GEOMETRY" ,   CART2D   ,
+  "BASIS"    ,   GLL      ,
+  "RULE"     ,   LL       ,
+  "VERBOSE"  ,   0        ,
+  "CHKPOINT" ,   0        ,
+  "OPTIMIZE" ,   1        ,
+  
+  /* -- Default integer values. */
+
+  "IO_FLD"   ,   1000,
+  "IO_HIS"   ,   10  ,
+  "IO_CFL"   ,   0   ,
+  "STEP_MAX" ,   500 ,
+  "N_POLY"   ,   5   ,
+  "N_TIME"   ,   2   ,
+  "N_Z"      ,   1   ,
+
+  0          ,   0.0
 };
 
-static struct {			/* Default options (global flags). */
-  char *name;
-  int   oval;
-} option_init[] = {
-  "BINARY"  ,  0         ,
-  "GEOM"    ,  CART2D    ,
-  "BASIS"   ,  GLL       ,
-  "RULE"    ,  GL        ,
-  "PROBLEM" ,  HELMHOLTZ ,
-  "VERBOSE" ,  0         ,
-  0         ,  0
-};
 
-static struct {			/* Default integer parameters. */
-  char *name;
-  int   ival;
-} iparam_init[] = {
-  0 , 0
-};
 
-static struct {			/* Default floating point parameters. */
-  char  *name;
-  double dval;
-} dparam_init[] = {
-  "BETA"   , 1.0 ,
-  "LAMBDA2", 0.0 ,
-  "KINVIS" , 1.0 ,
-  "RHO"    , 1.0 ,
-  0        , 0.0
-};
+
