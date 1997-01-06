8c103782295a375fe44deb5fbea529b8aeff2fda
hmb <>
1997-01-06 10:27:58 +0000

    Added t=0 as default.  Added some comments.

--- semtex/femlib/defaults.h
+++ semtex/femlib/defaults.h
@@ -17,18 +17,19 @@ static struct {
   "PHI"      ,   1.61803398874989484820 ,
   "E"        ,   2.71828182845904523536 ,
   "DEG"      ,  57.29577951308232087721 ,
-  "TWOPI"    ,   6.28318530717958647688 ,
   "PI"       ,   3.14159265358979323844 ,
+  "TWOPI"    ,   6.28318530717958647688 ,
   
   /* -- Default named parameters. */
 
-  "D_T"      ,   0.01  ,
-  "TOL_REL"  ,   1.0e-6,
-  "TOL_ABS"  ,   1.0e-6,
-  "BETA"     ,   1.0   ,	/* -- TWOPI / Lz. */
-  "LAMBDA2"  ,   0.0   ,	/* -- Helmholtz constant. */
-  "KINVIS"   ,   1.0   ,
-  "RHO"      ,   1.0   ,
+  "D_T"      ,   0.01  ,	/* -- Time step.                     */
+  "t"        ,   0.0   ,	/* -- Time.                          */
+  "TOL_REL"  ,   1.0e-6,	/* -- Relative tolerance.            */
+  "TOL_ABS"  ,   1.0e-6,	/* -- Absolute tolerance.            */
+  "BETA"     ,   1.0   ,	/* -- TWOPI / Lz (Fourier constant). */
+  "LAMBDA2"  ,   0.0   ,	/* -- Helmholtz constant.            */
+  "KINVIS"   ,   1.0   ,	/* -- Kinematic viscosity.           */
+  "RHO"      ,   1.0   ,	/* -- Density.                       */
 
   /* -- Option switches. */
 
