97fd6584788813ac6a8f6372e67fe8a970f9ce1d
hmb <>
1997-07-25 02:11:34 +0000

    Added planeSize, rounded to even version of nPlane, to supprt new FFT routines in femlib.

--- src/geometry.C
+++ src/geometry.C
@@ -11,11 +11,14 @@ RCSid[] = "$Id$";
 
 #include <Utility.h>
 #include <Geometry.h>
+#include <Femlib.h>
 
-int                Geometry::np   = 0;
-int                Geometry::nz   = 0;
-int                Geometry::nel  = 0;
-Geometry::CoordSys Geometry::csys = Geometry::Cartesian;
+
+int                Geometry::np    = 0;
+int                Geometry::nz    = 0;
+int                Geometry::nel   = 0;
+int                Geometry::psize = 0;
+Geometry::CoordSys Geometry::csys  = Geometry::Cartesian;
 
 
 void Geometry::set (const int      NP,
@@ -24,14 +27,32 @@ void Geometry::set (const int      NP,
 		    const CoordSys CS)
 // ---------------------------------------------------------------------------
 // Load values of static internal variables.
+//
+// NB: the value of psize is the value of nPlane, but rounded up to the
+// next even number in case nPlane is odd.  This is to simplify the handling
+// of Fourier transforms, which can be based on a real--complex transform
+// on some platforms.
 // ---------------------------------------------------------------------------
 {
   char routine[] = "Geometry::set", err[StrMax];
 
   np = NP; nz = NZ; nel = NE; csys = CS;
 
+  psize = nPlane();
+  if (psize & 1) psize++;
+
   if (nz > 1 && nz & 1) {
     sprintf (err, "nz must be even (%1d)", nz);
     message (routine, err, ERROR);
   }
+
+#if (defined(VECFFT))
+  int nfact, factors[32];
+  if (nz > 1) {
+    Femlib::primes23 (nz, nfact, factors);
+    if (!nfact) {
+      sprintf (err, "nz (%1d) must have prime factors 2 & 3", nz);
+      message (routine, err, ERROR);
+    }
+#endif
 }
