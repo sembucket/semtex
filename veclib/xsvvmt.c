42099450361d11f7539c97d3e9b7ddd81dc898c0
hmb <>
1997-07-31 02:03:50 +0000

    Changed for Fujitsu, explicit calc of loop indices.

--- veclib/xsvvmt.c
+++ veclib/xsvvmt.c
@@ -1,46 +1,38 @@
 /*****************************************************************************
- * xsvvmt:  z[i] = alpha * (x[i] - y[i]).                                    *
+ * xsvvmt:  z[i] = alpha * (x[i] - y[i]).
+ *
+ * $Id$
  *****************************************************************************/
 
+#ifdef __uxp__
+#pragma global novrec
+#pragma global noalias
+#endif
 
-void dsvvmt(int n, double alpha, const double *x, int incx,
-                                 const double *y, int incy,
-                                       double *z, int incz)
+
+void dsvvmt (int n, double alpha, const double *x, int incx,
+                                  const double *y, int incy,
+                                        double *z, int incz)
 {
   register int  i;
-
 
   x += (incx<0) ? (-n+1)*incx : 0;
   y += (incy<0) ? (-n+1)*incy : 0;
   z += (incz<0) ? (-n+1)*incz : 0;
   
-  for (i=0; i<n; i++) {
-    *z = alpha * (*x - *y);
-    x += incx;
-    y += incy;
-    z += incz;
-  }
+  for (i = 0; i < n; i++) z[i*incz] = alpha * (x[i*incx] - y[i*incy]);
 }
 
 
-
-
-
-void ssvvmt(int n, float alpha, const float *x, int incx,
-                                const float *y, int incy,
-                                      float *z, int incz)
+void ssvvmt (int n, float alpha, const float *x, int incx,
+                                 const float *y, int incy,
+                                       float *z, int incz)
 {
   register int  i;
-
 
   x += (incx<0) ? (-n+1)*incx : 0;
   y += (incy<0) ? (-n+1)*incy : 0;
   z += (incz<0) ? (-n+1)*incz : 0;
   
-  for (i=0; i<n; i++) {
-    *z = alpha * (*x - *y);
-    x += incx;
-    y += incy;
-    z += incz;
-  }
+  for (i = 0; i < n; i++) z[i*incz] = alpha * (x[i*incx] - y[i*incy]);
 }
