264502ed0cc017c0f650002b2553b36cf9fe84a0
hmb <>
1997-11-13 05:39:27 +0000

    This is the newer version with tensor invariants added.

--- utility/addfield.C
+++ utility/addfield.C
@@ -1,66 +1,67 @@
 //////////////////////////////////////////////////////////////////////////////
-// addfield.C: process sem field files, computing and adding vorticity,
-//             optionally divergence, helicity and enstrophy.
+// addfield.C: process sem field files, computing and adding vorticity and
+//            div, rate of strain tensor and invariants
 //
 // Usage:
 // -----
-// addfield [options] -s session session.fld
-//   options:
-//   -h     ... print this message
-//   -v     ... add vorticity (default)
-//   -e     ... add enstrophy and helicity (3D only)
-//   -d     ... add divergence
-//   -a     ... add vorticity/enstrophy/helicity/divergence
+// addvort -s session session.fld
 //////////////////////////////////////////////////////////////////////////////
 
-#include <Sem.h>
+#include "Sem.h"
 #include <new.h>
 #include <time.h>
 
 static char
 RCSid[] = "$Id$";
 
-static char prog[] = "addfield";
+static char  prog[] = "addfield";
 static void  memExhaust () { message ("new", "free store exhausted", ERROR); }
 
-static void    getargs  (integer, char**, char*&, char*&,
-			 integer&, integer&, integer&);
-static integer getDump  (Domain*, istream&);
-static void    putDump  (Domain*, vector<AuxField*>&, integer, ostream&);
+static void  getargs  (int, char**, char*&, char*&, int&, int&, int&, int&, int&);
+static int   getDump  (Domain*, istream&);
+static void  putDump  (Domain*, vector<AuxField*>&, int, ostream&);
 
 
-integer main (integer argc,
-	      char**  argv)
+int main (int argc, char *argv[])
 // ---------------------------------------------------------------------------
 // Driver.
 // ---------------------------------------------------------------------------
 {
+
   set_new_handler (&memExhaust);
 
   Geometry::CoordSys system;
-  char               *session, *dump, fields[StrMax];
-  integer            i, np, nz, nel, DIM;
-  ifstream           file;
-  FEML*              F;
-  Mesh*              M;
-  BCmgr*             B;
-  Domain*            D;
-  AuxField*          Ens;
-  AuxField*          Hel;
-  AuxField*          Div;
-  AuxField*          work;
-  integer            nData = 0;
+  char      *session, *dump, fields[StrMax];
+  int       i, j, np, nz, nel, DIM;
+  ifstream  file;
+  FEML*     F;
+  Mesh*     M;
+  BCmgr*    B;
+  Domain*   D;
+  AuxField* Ens;
+  AuxField* Hel;
+  AuxField* Div;
+  AuxField* InvQ;
+  AuxField* InvR;
+  AuxField* Disc;
+  AuxField* work;
+  int       nData = 0;
   vector <AuxField*> AuxPoint(3);
-  vector <AuxField*> dataField(10);
+  vector <AuxField*> dataField(12);
+  AuxField*** Sij;
 
-  // -- Set command line defaults.
+//  Geometry::CoordSys space=Geometry::system();
 
-  integer add_vort=0,
-          add_enst=0,
-          add_div =0;
+  // -- Set command line defaults
+
+  int add_vort=0,
+      add_enst=0,
+      add_div =0,
+      add_sij =0,
+      add_inv =0;
   
   Femlib::prep ();
-  getargs      (argc, argv, session, dump, add_vort, add_enst, add_div);
+  getargs      (argc, argv, session, dump, add_vort, add_enst, add_div, add_sij, add_inv);
 
   // -- Set up domain.
 
@@ -69,9 +70,9 @@ integer main (integer argc,
   B = new BCmgr  (*F);
 
   nel    = M -> nEl();  
-  np     =  (integer) Femlib::value ("N_POLY");
-  nz     =  (integer) Femlib::value ("N_Z"   );
-  system = ((integer) Femlib::value ("CYLINDRICAL") ) ?
+  np     =  (int) Femlib::value ("N_POLY");
+  nz     =  (int) Femlib::value ("N_Z"   );
+  system = ((int) Femlib::value ("CYLINDRICAL") ) ?
                      Geometry::Cylindrical : Geometry::Cartesian;
   
   Geometry::set (np, nz, nel, system);
@@ -87,6 +88,18 @@ integer main (integer argc,
 
   file.open (dump);
 
+  // we want the velocity gradient tensor irrespective of
+  // what fields we want to output
+
+  AuxField*** Vij = new AuxField** [DIM];
+  for ( i = 0 ; i < DIM ; i++) {
+    Vij[i] = new AuxField* [DIM];
+    for ( j = 0 ; j < DIM ; j++ ) {
+      Vij[i][j] = new AuxField (D -> Esys);
+      *Vij[i][j] = 0.0;
+    }
+  }
+  
   // -- Create workspace and dataField storage areas.
 
   work = new AuxField (D -> Esys);
@@ -97,87 +110,151 @@ integer main (integer argc,
     else
       for (i = 0; i < DIM; i++)
 	vorticity [i] = new AuxField (D -> Esys, 'r' + i);
-    if (add_vort == 1) {
-      for (i = 0; i < 2*DIM - 3; i++) dataField(i) = vorticity[i];
+    if(add_vort == 1) {
+      for( i = 0 ; i < 2*DIM - 3 ; i++ ) dataField(i) = vorticity[i];
       nData += 2*DIM - 3;
     }
-    if (add_enst == 1) {
-      dataField(nData)   = new AuxField (D -> Esys, 'e');
-      Ens = dataField(nData);
-
-      dataField(nData+1) = new AuxField (D -> Esys, 'h');
-      Hel = dataField(nData+1);
-
+    if(add_enst == 1) {
+      dataField(nData)   = new AuxField (D -> Esys, 'e'); Ens = dataField(nData);
+      dataField(nData+1) = new AuxField (D -> Esys, 'h'); Hel = dataField(nData+1);
       nData += 2;
     }
   }
-  
-  if (add_div == 1) {
-    dataField(nData) = new AuxField (D -> Esys, 'd');
-    Div = dataField(nData);
 
+  if(add_div+add_inv > 0) {
+    dataField(nData) = new AuxField (D -> Esys, 'd'); Div = dataField(nData);
     nData += 1;
   }
   
-  // -- Cycle through field dump, computing vorticity, writing output.
-  // -- Musn't change the order below because we are relying on the
-  // -- inverse transform being done BEFORE dumping enstrophy/helicity.
+  int iadd = 0;
+
+  if( add_sij+add_inv > 0 ) {
+    Sij = new AuxField** [DIM];
+    for ( i = 0 ; i < DIM ; i++) {
+      Sij[i] = new AuxField* [DIM];
+      for ( j = 0 ; j < DIM ; j++ ) {
+	if( i <= j ) {
+	  Sij[i][j] = new AuxField (D -> Esys, 'i' + iadd);
+	  iadd ++;
+	}
+	else
+	  Sij[i][j] = Sij[j][i];
+      }
+    }
+    if( add_sij == 1 ){
+      for ( i = 0 ; i < DIM ; i++) {
+	for ( j = i ; j < DIM ; j++ ) {
+	  dataField(nData) = Sij[i][j];
+	  nData += 1;
+	}
+      }
+    }
+    if( add_inv == 1 ) {
+      dataField(nData)   = new AuxField (D -> Esys, 'Q'); InvQ = dataField(nData);   	*InvQ = 0.0;
+      dataField(nData+1) = new AuxField (D -> Esys, 'R'); InvR = dataField(nData+1); 	*InvR = 0.0;
+      dataField(nData+2) = new AuxField (D -> Esys, 'L'); Disc = dataField(nData+2); 	*Disc = 0.0;
+      nData += 3;
+    }
+  }
+  
+  // -- Cycle through field dump, first computing the velocity gradient 
+  // -- tensor and then the other quantities - vorticity etc. Then write
+  // -- output defined in the dataField AuxFields. musn't change the order
+  // -- below because we are relying on the inverse transform being done
+  // -- BEFORE dumping enstrophy/helicity because inner products are done
+  // -- in physical space
+  
   
   while (getDump (D, file)) {
     
-    if (DIM > 2 ) D -> transform (+1);
-    
-    if (add_div == 1)  {
-      
-      *Div = 0.0;
-      
-      if(system == Geometry::Cartesian)
-	for( i = 0 ; i< DIM ; i++) {
-	  (*work = *D -> u[i]) . gradient (i); *Div += *work;
-	}
-      else {
-	(*work = *D -> u[0]) . gradient (0);                   *Div += *work;
-	(*work = *D -> u[1]) . mulR() . gradient (1) . divR(); *Div += *work;
-	if( DIM == 3 ) {
-	  (*work = *D -> u[2]) . gradient (2) . divR();        *Div += *work;
-	}	  
-	D -> u[0] -> smooth (Div);
+    if( DIM > 2 ) D -> transform (+1);
+
+    // Velocity gradient tensor - calculated in Fourier, then transformed back
+
+    for ( i = 0 ; i < DIM ; i++ ) {
+      for ( j = 0 ; j < DIM ; j++ ) {
+	(*Vij[i][j] = *D -> u[i]) . gradient (j);
+	D -> u[0] -> smooth (Vij[i][j]);
+	Vij[i][j] -> transform(-1);
       }
-      if ( DIM > 2) Div -> transform (-1);
+    }
+
+    if( DIM > 2 ) D -> transform (-1);
+
+    if(system == Geometry::Cylindrical && DIM == 3) {
+      
+      for( i=0; i<DIM; i++ ) Vij[i][2] -> divR();
+      (*work = *D -> u[2]) . divR(); *Vij[1][2] -= *work;
+      (*work = *D -> u[1]) . divR(); *Vij[2][2] += *work;
+
+    }
+	
+    if(add_div + add_inv > 0)  {
+      *Div = 0.0;
+      for ( i = 0 ; i < DIM ; i++ ) *Div += *Vij[i][i];
     }
     
-    if (add_vort+add_enst > 0) {
+    if(add_vort+add_enst > 0) {
       
       if (DIM == 2) {
-	(*work = *D -> u[1]) . gradient (0); *vorticity[0]  = *work;
-	(*work = *D -> u[0]) . gradient (1); *vorticity[0] -= *work;
-	D -> u[0] -> smooth (vorticity [0]);
+	*vorticity[0]  = *Vij[1][0];
+	*vorticity[0] -= *Vij[0][1];
       } else {
-	if(system == Geometry::Cartesian) {
-	  (*work = *D -> u[2]) . gradient (1); *vorticity[0]  = *work;
-	  (*work = *D -> u[1]) . gradient (2); *vorticity[0] -= *work;
-	  (*work = *D -> u[0]) . gradient (2); *vorticity[1]  = *work;
-	  (*work = *D -> u[2]) . gradient (0); *vorticity[1] -= *work;
-	}
-	else {
-	  (*work = *D -> u[2]) . mulR() . gradient (1); *vorticity[0]  = *work;
-	  (*work = *D -> u[1]) . gradient (2);          *vorticity[0] -= *work;
-          vorticity[0] -> divR();
-	  
-	  (*work = *D -> u[0]) . gradient (2) . divR(); *vorticity[1]  = *work;
-	  (*work = *D -> u[2]) . gradient (0);          *vorticity[1] -= *work;
-	}
-	(*work = *D -> u[1]) . gradient (0); *vorticity[2]  = *work;
-	(*work = *D -> u[0]) . gradient (1); *vorticity[2] -= *work;
-	for (i = 0; i < DIM; i++) D -> u[i] -> smooth (vorticity [i]);
-	vorticity [0] -> transform (-1);
-	vorticity [1] -> transform (-1);
-	vorticity [2] -> transform (-1);
+	*vorticity[0]  = *Vij[2][1];
+	*vorticity[0] -= *Vij[1][2];
+	*vorticity[1]  = *Vij[0][2];
+	*vorticity[1] -= *Vij[2][0];
+	*vorticity[2]  = *Vij[1][0];
+	*vorticity[2] -= *Vij[0][1];
       }
-      
     }
-    
-    D -> transform (-1);
+
+    if( add_sij+add_inv > 0 ) {
+      
+      for( i=0; i<DIM; i++ ) {
+	for( j=1; j<DIM; j++ ) {
+	  *Sij[i][j]  = *Vij[i][j];
+	  *Sij[i][j] += *Vij[j][i];
+	  *Sij[i][j] *= 0.5;
+	}
+      }
+
+      if( add_inv == 1 ) {
+
+	// 2nd invariant (Q from Chong et al.)
+
+	InvQ -> times      (*Vij[0][0],*Vij[1][1]);
+	InvQ -> timesMinus (*Vij[0][1],*Vij[1][0]);
+	InvQ -> timesPlus  (*Vij[0][0],*Vij[2][2]);
+	InvQ -> timesMinus (*Vij[0][2],*Vij[2][0]);
+	InvQ -> timesPlus  (*Vij[1][1],*Vij[2][2]);
+	InvQ -> timesMinus (*Vij[1][2],*Vij[2][1]);
+
+	// 3rd invariant - determinant of Vij (R from Chong et al.)
+
+	work -> times      (*Vij[1][1],*Vij[2][2]);
+	work -> timesMinus (*Vij[2][1],*Vij[1][2]);
+	InvR -> times      (*work,     *Vij[0][0]);
+
+	work -> times      (*Vij[1][2],*Vij[2][0]);
+	work -> timesMinus (*Vij[2][2],*Vij[1][0]);
+	InvR -> timesPlus  (*work,     *Vij[0][1]);
+
+	work -> times      (*Vij[2][1],*Vij[1][0]);
+	work -> timesMinus (*Vij[1][1],*Vij[2][0]);
+	InvR -> timesPlus  (*work,     *Vij[0][2]);
+
+
+	// Discriminant of Vij (N.B. DIVERGENCE (P from Chong et al.) ASSUMED = 0)
+
+	work -> times (*InvQ,*InvQ);
+	Disc -> times (*work,*InvQ);
+	work -> times (*InvR,*InvR);
+	*work *= 6.75;
+	*Disc += *work;
+
+      }
+    }    
     
     if(add_enst == 1) {
       Ens -> innerProduct(vorticity,vorticity);
@@ -195,13 +272,16 @@ integer main (integer argc,
 }
 
 
-static void getargs (integer  argc    ,
-		     char**   argv    ,
-		     char*&   session ,
-		     char*&   dump    ,
-		     integer& add_vort,
-		     integer& add_enst,
-		     integer& add_div )
+static void getargs (int argc       ,
+		     char** argv    ,
+		     char*& session ,
+		     char*& dump    ,
+		     int&   add_vort,
+		     int&   add_enst,
+		     int&   add_div ,
+		     int&   add_sij ,
+                     int&   add_inv )
+
 // ---------------------------------------------------------------------------
 // Deal with command-line arguments.
 // ---------------------------------------------------------------------------
@@ -212,12 +292,15 @@ static void getargs (integer  argc    ,
                    "  -v   ... add vorticity (default) \n"
 		   "  -e   ... add enstrophy and helicity (3D only) \n"
 		   "  -d   ... add divergence\n"
-		   "  -a   ... add vorticity/enstrophy/helicity/divergence\n";
+		   "  -t   ... add rate of strain tensor Sij\n"
+		   "  -i   ... add invariants of Sij (but NOT Sij itself) \n"
+		   "           (NB - Divergence is ASSUMED equal to zero) \n"
+		   "  -a   ... add them all \n";
 
-  char buf[StrMax];
+  char buf[StrMax], c;
  
   while (--argc  && **++argv == '-')
-    switch (*++argv[0]) {
+    switch (c = *++argv[0]) {
     case 'h':
       sprintf (buf, usage, prog);
       cout << buf;
@@ -240,10 +323,19 @@ static void getargs (integer  argc    ,
     case 'd':
       add_div = 1;
       break;
+    case 't':
+      add_sij = 1;
+      break;
+    case 'i':
+      add_inv = 1;
+      add_div = 1;
+      break;
     case 'a':
       add_enst = 1;
       add_vort = 1;
       add_div = 1;
+      add_sij = 1;
+      add_inv = 1;
       break;
 
     default:
@@ -253,7 +345,7 @@ static void getargs (integer  argc    ,
       break;
     }
 
-  if (add_vort+add_enst+add_div == 0) add_vort=1;
+  if(add_vort+add_enst+add_div+add_inv+add_sij == 0) message (prog, "No fields to add", ERROR);
 
   if   (!session)  message (prog, "no session file", ERROR);
   if   (argc != 1) message (prog, "no field file",   ERROR);
@@ -261,8 +353,8 @@ static void getargs (integer  argc    ,
 }
 
 
-static integer getDump (Domain*  D   ,
-			istream& dump)
+static int getDump (Domain*  D   ,
+		    istream& dump)
 // ---------------------------------------------------------------------------
 // Read next set of field dumps from file.
 // ---------------------------------------------------------------------------
@@ -272,10 +364,10 @@ static integer getDump (Domain*  D   ,
 }
 
 
-static void putDump  (Domain*            D       ,
+static void putDump  (Domain*            D    ,
 		      vector<AuxField*>& outField,
-		      integer            nOut    ,
-		      ostream&           strm    )
+		      int                nOut,
+		      ostream&           strm)
 // ---------------------------------------------------------------------------
 // This is a version of the normal Domain dump that adds extra AuxFields.
 // ---------------------------------------------------------------------------
@@ -293,8 +385,8 @@ static void putDump  (Domain*            D       ,
     "%-25s "    "Format\n"
   };
 
-  integer       i;
-  const integer DIM = (D -> nField() == 3) ? 2 : 3;
+  int       i;
+  const int DIM = (D -> nField() == 3) ? 2 : 3;
   char      routine[] = "putDump";
   char      s1[StrMax], s2[StrMax];
   time_t    tp (::time (0));
