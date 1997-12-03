ef8e671822fe4a26b818c5bfc4c93d981b1b7b92
hmb <>
1997-12-03 23:33:04 +0000

    Incorporated NEC FFT routines for SX-4./

--- semtex/femlib/fourier.c
+++ semtex/femlib/fourier.c
@@ -44,7 +44,7 @@ void sDFTr (float*        data,
  *     the real and imaginary parts for each mode alternate (both cases).
  * ------------------------------------------------------------------------- */
 {
-  char             routine[] = "sDFTr";
+  const char       routine[] = "sDFTr";
   const integer    ntot = tlen * ntrn;
   register integer i;
   integer          dum, ip, iq, ir, ipqr2;
@@ -136,34 +136,42 @@ void dDFTr (double*       data,
  * Double-precision version of sDFTr.
  * ------------------------------------------------------------------------- */
 {
-  char             routine[] = "dDFTr";
+  const char       routine[] = "dDFTr";
   const integer    ntot = tlen * ntrn;
   register integer i;
-  integer          dum, ip, iq, ir, ipqr2;
+  integer          dum, ip, iq, ir, ipqr2, *ifax;
   register double  *work, *Wtab, *ptr;
 
   if (tlen < 2 || !ntrn) return;
 
-#if 1
+#if defined(_SX)  /* -- Use NEC FFT routines. */
 
-  /* -- Temperton FFT routine is default. */
-
-  dum = tlen;
-  prf235 (&dum, &ip, &iq, &ir, &ipqr2);
-  
-  if (!dum    ) message (routine, "tlen needs prime factors 2, 3, 5", ERROR);
-  if (ntrn & 1) message (routine, "ntrn must be even",                ERROR);
-
-  work = dvector (0, ntot + ipqr2 - 1);
+  ifax = ivector (0, 63);
+  work = dvector (0, ntot + tlen - 1);
   Wtab = work + ntot;
 
-  dsetpf (Wtab, tlen, ip, iq, ir);
-  dmpfft (data, work, ntrn, tlen, ip, iq, ir, Wtab, sign);
-  if (sign == +1) dscal (ntot, 1.0 / tlen, data, 1);
+  rftfax (tlen, ifax, Wtab);
 
+  if (ifax[0] == -99)
+    message (routine, "tlen needs prime factors 2, 3, 5", ERROR);
+
+  if (sign == +1) {
+    rfft  (data, work, Wtab, ifax, tlen, ntrn, 1.0 / (double) tlen);
+    dcopy ((tlen - 2) * ntrn, data +              ntrn, 1, work,            1);
+    dcopy (             ntrn, data + (tlen - 1) * ntrn, 1, data + ntrn,     1);
+    dcopy ((tlen - 2) * ntrn, work,                     1, data + 2 * ntrn, 1);
+  } else {
+    dcopy ((tlen - 2) * ntrn, data + 2 * ntrn, 1, work,                     1);
+    dcopy (             ntrn, data + ntrn,     1, data + (tlen - 1) * ntrn, 1);
+    dcopy ((tlen - 2) * ntrn, work,            1, data +              ntrn, 1);
+    rfft  (data, work, Wtab, ifax, tlen, ntrn, -1.0);
+  }
+
+  freeIvector (ifax, 0);
   freeDvector (work, 0);
 
-#else
+#elif defined(DEBUG)  /* -- Unvectorized FFTPACK routines. */
+
   work = dvector (0, 3 * tlen + 14);
   Wtab = work + tlen;
   ptr  = data;
@@ -215,5 +223,25 @@ void dDFTr (double*       data,
   }
   
   freeDvector (work, 0);
+
+#else
+
+  /* -- Temperton FFT routine is default. */
+
+  dum = tlen;
+  prf235 (&dum, &ip, &iq, &ir, &ipqr2);
+  
+  if (!dum    ) message (routine, "tlen needs prime factors 2, 3, 5", ERROR);
+  if (ntrn & 1) message (routine, "ntrn must be even",                ERROR);
+
+  work = dvector (0, ntot + ipqr2 - 1);
+  Wtab = work + ntot;
+
+  dsetpf (Wtab, tlen, ip, iq, ir);
+  dmpfft (data, work, ntrn, tlen, ip, iq, ir, Wtab, sign);
+  if (sign == +1) dscal (ntot, 1.0 / tlen, data, 1);
+
+  freeDvector (work, 0);
+
 #endif
 }
