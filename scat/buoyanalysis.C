3b5ce1b183e7e7be7ac523924711bebc1ff76fb2
hmb <>
1998-01-27 23:43:38 +0000

    Fixed error in which field was used for flux calc, also added lift & drag force computation & output for "wall" group.

--- scat/buoyanalysis.C
+++ scat/buoyanalysis.C
@@ -22,8 +22,11 @@ BuoyAnalyser::BuoyAnalyser (Domain& D   ,
     // -- Open state-variable file.
 
     flx_strm.open (strcat (strcpy (str, src.name), ".flx"));
+    if (!flx_strm) message (routine, "can't open flux file",  ERROR);
 
-    if (!flx_strm) message (routine, "can't open flux file", ERROR);
+    flx_strm << "# Buoy state information file"          << endl;
+    flx_strm << "# Step Time Flux [Fpre Fvis Ftot]-axis" << endl;
+    flx_strm << "# ------------------------------------" << endl;
   }
 }
 
@@ -33,9 +36,9 @@ void BuoyAnalyser::analyse (AuxField*** work)
 // Step-by-step processing.
 // ---------------------------------------------------------------------------
 {
- const integer DIM = Geometry::nDim();
+  const integer DIM = Geometry::nDim();
 
- Analyser::analyse (work);
+  Analyser::analyse (work);
 
   ROOTONLY {
     const integer periodic = !(src.step % (integer)Femlib::value ("IO_HIS")) ||
@@ -45,7 +48,23 @@ void BuoyAnalyser::analyse (AuxField*** work)
 
     if (!state) return;
 
-    flx_strm << src.step << " " << src.time;
-    flx_strm << Field::flux(src.u[DIM+1]) << endl;
+    real   flux;
+    Vector pfor, vfor, tfor;
+    char   s[StrMax];
+
+    flux   = Field::flux            (src.u[DIM]);
+    pfor   = Field::normalTraction  (src.u[DIM + 1]);
+    vfor   = Field::tangentTraction (src.u[0], src.u[1]);
+    tfor.x = pfor.x + vfor.x;
+    tfor.y = pfor.y + vfor.y;
+
+    sprintf (s,
+	     "%#6d %#10.6g %#10.6g "
+	     "%#10.6g %#10.6g %#10.6g %#10.6g %#10.6g %#10.6g",
+	     src.step, src.time, flux  ,
+	     pfor.x,   vfor.x,   tfor.x,
+	     pfor.y,   vfor.y,   tfor.y);
+
+    flx_strm << s << endl;
   }
 }
