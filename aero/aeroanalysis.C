0de84105c2b5b49495e3fef04374fac6ffcb4a7c
hmb <>
1998-08-14 03:06:21 +0000

    Added code for output of forces at each axial station in 3D, every IO_HIS steps.

--- aero/aeroanalysis.C
+++ aero/aeroanalysis.C
@@ -32,6 +32,18 @@ AeroAnalyser::AeroAnalyser (Domain& D   ,
     sta_strm << "# Aero state information file"                 << endl;
     sta_strm << "# Step Time [pos vel acc Fpre Fvis Ftot]-axis" << endl;
     sta_strm << "# -------------------------------------------" << endl;
+
+#if defined(FORCES)
+    // -- Open force file.
+
+    for_strm.open (strcat (strcpy (str, src.name), ".for"));
+
+    if (!for_strm) message (routine, "can't open force file", ERROR);
+
+    for_strm << "# Aero force information file" << endl;
+    for_strm << "# Time ZPos  Fx  Fy  Fz"       << endl;
+    for_strm << "# ---------------------------" << endl;
+#endif
   }
 }
 
@@ -52,5 +64,106 @@ void AeroAnalyser::analyse (AuxField*** work)
     if (!state) return;
 
     sta_strm << src.step << " " << src.time << body << endl;
+  }
+
+#if defined(FORCES)
+  const integer forstep = (integer) Femlib::value ("IO_HIS");
+
+  if (forstep && !(src.step % forstep) && Geometry::nDim() == 3) forceDist();
+#endif
+}
+
+
+void AeroAnalyser::forceDist ()
+// ---------------------------------------------------------------------------
+// Compute forces at each axial location, output on root processor.
+// Designed only to be called for 3D.
+// ---------------------------------------------------------------------------
+{
+  register integer i, k;
+  const integer    nZ     = Geometry::nZ();
+  const integer    nProc  = Geometry::nProc();
+  const integer    nZProc = Geometry::nZProc();
+  Field*           master = src.u[0];
+  vector<real>     work (5 * nZProc);
+  register real    *px = work(),
+                   *py = px + nZProc,
+                   *vx = py + nZProc,
+                   *vy = vx + nZProc,
+                   *vz = vy + nZProc;
+
+  // -- Fill local copy of force data.
+
+  Veclib::zero (5 * nZProc, px, 1);
+  master -> normTractionV (px, py,     src.u[3]);
+  master -> tangTractionV (vx, vy, vz, src.u[0], src.u[1], src.u[2]);
+  Veclib::vadd (nZProc, vx, 1, px, 1, vx, 1);
+  Veclib::vadd (nZProc, vy, 1, py, 1, vy, 1);
+
+  // -- Order data, inverse Fourier transform, print up.
+
+  if (nProc > 1) {
+
+    ROOTONLY {
+      vector<real>  force (4 * nZ);	// -- Even number forced by DFTr.
+      register real *x = force(), *y = x + 1, *z = y + 1, *t = z + 1;
+      const real    dz = Femlib::value ("TWOPI / BETA / N_Z");
+
+      for (i = 0; i < nZProc; i++) {
+	x[4 * i] = vx[i];
+	y[4 * i] = vy[i];
+	z[4 * i] = vz[i];
+	t[4 * i] = 0.0;
+      }
+	
+      for (k = 1; k < nProc; k++) {
+	Femlib::recv (vx, 3 * nZProc, k);
+	for (i = 0; i < nZProc; i++) {
+	  x[4 * (i + k * nZProc)] = vx[i];
+	  y[4 * (i + k * nZProc)] = vy[i];
+	  z[4 * (i + k * nZProc)] = vz[i];
+	  t[4 * (i + k * nZProc)] = 0.0;
+	}
+      }
+      
+      Femlib::DFTr (x, nZ, 4, -1);
+
+      for (k = 0; k < nZ; k++)
+	for_strm << setw(10) << src.time 
+		 << setw(15) << k * dz
+		 << setw(15) << x[4 * k]
+		 << setw(15) << y[4 * k]
+		 << setw(15) << z[4 * k]
+		 << endl;
+
+      for_strm.flush();
+
+    } else
+      Femlib::send (vx, 3 * nZProc, 0);
+
+  } else {			// -- Serial version.
+
+    vector<real>  force (4 * nZ);
+    register real *x = force(), *y = x + 1, *z = y + 1, *t = z + 1;
+    const real    dz = Femlib::value ("TWOPI / BETA / N_Z");
+
+    for (k = 0; k < nZ; k++) {
+      x[4 * k] = vx[k];
+      y[4 * k] = vy[k];
+      z[4 * k] = vz[k];
+      t[4 * k] = 0.0;
+    }
+	
+    Femlib::DFTr (x, nZ, 4, -1);
+
+    for (k = 0; k < nZ; k++)
+      for_strm << setw(10) << src.time 
+	       << setw(15) << k * dz
+	       << setw(15) << x[4 * k]
+	       << setw(15) << y[4 * k]
+	       << setw(15) << z[4 * k]
+	       << endl;
+
+    for_strm.flush();
   }
 }
