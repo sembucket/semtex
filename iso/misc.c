17c06eb2d69e77550dcce4e7b5df8448f49d6f26
hmb <>
1999-03-19 20:47:09 +0000

    Changed macro usage to improve performance a little.

--- iso/misc.c
+++ iso/misc.c
@@ -6,11 +6,12 @@
 
 #include "iso.h"
 
-#define PEQ(Z1,c,Z2)  (Z1)->Re +=  (c) * (Z2)->Re; (Z1)->Im +=  (c) * (Z2)->Im
-#define NEQ(Z1,c,Z2)  (Z1)->Re -=  (c) * (Z2)->Re; (Z1)->Im -=  (c) * (Z2)->Im
+#define PEQ(Z1,c,Z2)  (Z1)->Re += (c) * (Z2)->Re; (Z1)->Im += (c) * (Z2)->Im
+#define NEQ(Z1,c,Z2)  (Z1)->Re -= (c) * (Z2)->Re; (Z1)->Im -= (c) * (Z2)->Im
 
 
-void  zeroVF (CVF Z, const int* Dim)
+void zeroVF (CVF        Z  ,
+	     const int* Dim)
 /* ------------------------------------------------------------------------- *
  * Zero all information in Z.
  * ------------------------------------------------------------------------- */
@@ -23,36 +24,41 @@ void  zeroVF (CVF Z, const int* Dim)
 }
 
 
-void  zeroF (CF Z, const int* Dim)
+void zeroF (CF         Z  ,
+	    const int* Dim)
 /* ------------------------------------------------------------------------- *
  * Zero all information in Z.
  * ------------------------------------------------------------------------- */
 {
-  const    int   Npts = 2 * Dim[1] * Dim[2] * Dim[3];
+  const int Npts = 2 * Dim[1] * Dim[2] * Dim[3];
 
   memset (&Z[0][0][0].Re, '\0', Npts * sizeof (real));
 }
 
 
-void  copyF (CF to, const CF from, const int* Dim)
+void copyF (CF         tgt,
+	    const CF   src,
+	    const int* Dim)
 /* ------------------------------------------------------------------------- *
  * Copy one scalar field to another.
  * ------------------------------------------------------------------------- */
 {
-  const    int   Npts = 2 * Dim[1] * Dim[2] * Dim[3];
+  const int Npts = 2 * Dim[1] * Dim[2] * Dim[3];
 
-  memcpy (&to[0][0][0].Re, &from[0][0][0].Re, Npts * sizeof (real));
+  memcpy (&tgt[0][0][0].Re, &src[0][0][0].Re, Npts * sizeof (real));
 }
 
 
-void  scaleF (CF U, const real alpha, const int* Dim)
+void scaleF (CF         U    ,
+	     const real alpha,
+	     const int* Dim  )
 /* ------------------------------------------------------------------------- *
  * Scale U by alpha.
  * ------------------------------------------------------------------------- */
 {
-  register int    i;
-  register real*  u = &U[0][0][0].Re;
-  const    int    Npts = 2 * Dim[1] * Dim[2] * Dim[3];
+  register int   i;
+  register real* u = &U[0][0][0].Re;
+  const int      Npts = 2 * Dim[1] * Dim[2] * Dim[3];
 
   for (i = 0; i < Npts; i++) {
     *u *= alpha;
@@ -61,16 +67,17 @@ void  scaleF (CF U, const real alpha, const int* Dim)
 }
 
 
-void setF (CF f1, const CF f2, const int* Dim)
+void setF (CF         f1 ,
+	   const CF   f2 ,
+	   const int* Dim)
 /* ------------------------------------------------------------------------- *
  * Set field f1 = f2, over truncated space.
  * ------------------------------------------------------------------------- */
 {
-  register int  k1, k2, k3, b1, b2;
-  const    int  N = Dim[1];
-  const    int  K = Dim[3];
-  const    int  FOURKon3 = (4 * K) / 3;
-
+  register int k1, k2, k3, b1, b2;
+  const int    N = Dim[1];
+  const int    K = Dim[3];
+  const int    FOURKon3 = (4 * K) / 3;
 
   f1[ 0][ 0][ 0].Re = f2[ 0][ 0][ 0].Im;
 
@@ -98,7 +105,9 @@ void setF (CF f1, const CF f2, const int* Dim)
 }
  
 
-void addF (CF f1, const CF f2, const int* Dim)
+void addF (CF         f1 ,
+	   const CF   f2 ,
+	   const int* Dim)
 /* ------------------------------------------------------------------------- *
  * Set field f1 += f2, over truncated space.
  * ------------------------------------------------------------------------- */
@@ -107,7 +116,6 @@ void addF (CF f1, const CF f2, const int* Dim)
   const    int  N = Dim[1];
   const    int  K = Dim[3];
   const    int  FOURKon3 = (4 * K) / 3;
-
 
   f1[ 0][ 0][ 0].Re += f2[ 0][ 0][ 0].Re;
 
@@ -148,16 +156,17 @@ void addF (CF f1, const CF f2, const int* Dim)
 }
  
 
-void subF (CF f1, const CF f2, const int* Dim)
+void subF (CF         f1 ,
+	   const CF   f2 ,
+	   const int* Dim)
 /* ------------------------------------------------------------------------- *
  * Set field f1 -= f2, over truncated space.
  * ------------------------------------------------------------------------- */
 {
-  register int  k1, k2, k3, b1, b2;
-  const    int  N = Dim[1];
-  const    int  K = Dim[3];
-  const    int  FOURKon3 = (4 * K) / 3;
-
+  register int k1, k2, k3, b1, b2;
+  const int    N = Dim[1];
+  const int    K = Dim[3];
+  const int    FOURKon3 = (4 * K) / 3;
 
   f1[ 0][ 0][ 0].Re -= f2[ 0][ 0][ 0].Re;
 
@@ -198,18 +207,21 @@ void subF (CF f1, const CF f2, const int* Dim)
 }
 
 
-void  project (CVF G,  CF W,  const int* Dim)
+void project (CVF        G  ,
+	      CF         W  ,
+	      const int* Dim)
 /* ------------------------------------------------------------------------- *
  * Make divergence-free projection of G, using W as workspace.
  *
  *                       Gj = Gj - kj*(kiGi)/k^2.
  * ------------------------------------------------------------------------- */
 {
-  register int  k1, b1, k2, b2, k3;
-  register real kSqrd;
-  const    int  N        = Dim[1];
-  const    int  K        = Dim[3];
-  const    int  FOURKon3 = (4 * K) / 3;
+  register int     k1, b1, k2, b2, k3;
+  register real    c1, c2, c3, kSqrd;
+  register complex *f, *g;
+  const int        N        = Dim[1];
+  const int        K        = Dim[3];
+  const int        FOURKon3 = (4 * K) / 3;
 
   /* -- Use W to construct kiGi. */
   
@@ -218,43 +230,44 @@ void  project (CVF G,  CF W,  const int* Dim)
   for (k1 = 1; k1 < K; k1++) {
     b1 = N - k1;
 
-    PEQ (W[k1][ 0]+ 0, k1, G[1][k1][ 0]+ 0);
-    PEQ (W[ 0][k1]+ 0, k1, G[2][ 0][k1]+ 0);
-    PEQ (W[ 0][ 0]+k1, k1, G[3][ 0][ 0]+k1);
+    f = W[k1][ 0];    g = G[1][k1][ 0];    PEQ (f, k1, g);
+    f = W[ 0][k1];    g = G[2][ 0][k1];    PEQ (f, k1, g);
+    f = W[ 0][ 0]+k1; g = G[3][ 0][ 0]+k1; PEQ (f, k1, g);
 
     for (k2 = 1; k2 < K && k1+k2 <= FOURKon3; k2++) {
       b2 = N - k2;
 
-      PEQ (W[k1][k2]+ 0,  k1, G[1][k1][k2]+ 0); /* kk0 face. */
-      PEQ (W[k1][k2]+ 0,  k2, G[2][k1][k2]+ 0);
-      PEQ (W[b1][k2]+ 0, -k1, G[1][b1][k2]+ 0); /* bk0 face. */
-      PEQ (W[b1][k2]+ 0,  k2, G[2][b1][k2]+ 0);
-      PEQ (W[k1][ 0]+k2,  k1, G[1][k1][ 0]+k2); /* k0k face. */
-      PEQ (W[k1][ 0]+k2,  k2, G[3][k1][ 0]+k2);
-      PEQ (W[b1][ 0]+k2, -k1, G[1][b1][ 0]+k2); /* b0k face. */
-      PEQ (W[b1][ 0]+k2,  k2, G[3][b1][ 0]+k2);
-      PEQ (W[ 0][k1]+k2,  k1, G[2][ 0][k1]+k2); /* 0kk face. */
-      PEQ (W[ 0][k1]+k2,  k2, G[3][ 0][k1]+k2);
-      PEQ (W[ 0][b1]+k2, -k1, G[2][ 0][b1]+k2); /* 0bk face. */
-      PEQ (W[ 0][b1]+k2,  k2, G[3][ 0][b1]+k2);
-      
+      f = W[k1][k2];    g = G[1][k1][k2];    PEQ (f,  k1, g);
+      f = W[k1][k2];    g = G[2][k1][k2];    PEQ (f,  k2, g);
+      f = W[b1][k2];    g = G[1][b1][k2];    PEQ (f, -k1, g);
+      f = W[b1][k2];    g = G[2][b1][k2];    PEQ (f,  k2, g);
+      f = W[k1][ 0]+k2; g = G[1][k1][ 0]+k2; PEQ (f,  k1, g);
+      f = W[k1][ 0]+k2; g = G[3][k1][ 0]+k2; PEQ (f,  k2, g);
+      f = W[b1][ 0]+k2; g = G[1][b1][ 0]+k2; PEQ (f, -k1, g);
+      f = W[b1][ 0]+k2; g = G[3][b1][ 0]+k2; PEQ (f,  k2, g);
+      f = W[ 0][k1]+k2; g = G[2][ 0][k1]+k2; PEQ (f,  k1, g);
+      f = W[ 0][k1]+k2; g = G[3][ 0][k1]+k2; PEQ (f,  k2, g);
+      f = W[ 0][b1]+k2; g = G[2][ 0][b1]+k2; PEQ (f, -k1, g);
+      f = W[ 0][b1]+k2; g = G[3][ 0][b1]+k2; PEQ (f,  k2, g);
+
       for (k3 = 1; k3 < K && k2+k3 <= FOURKon3 && k1+k3 <= FOURKon3; k3++) {
 
-	PEQ (W[k1][k2]+k3,  k1, G[1][k1][k2]+k3);
-	PEQ (W[k1][k2]+k3,  k2, G[2][k1][k2]+k3);
-	PEQ (W[k1][k2]+k3,  k3, G[3][k1][k2]+k3);
+	f = W[k1][k2]+k3; g = G[1][k1][k2]+k3;	PEQ (f,  k1, g);
+	f = W[k1][k2]+k3; g = G[2][k1][k2]+k3;	PEQ (f,  k2, g);
+	f = W[k1][k2]+k3; g = G[3][k1][k2]+k3;	PEQ (f,  k3, g);
+	
+	f = W[b1][k2]+k3; g = G[1][b1][k2]+k3;	PEQ (f, -k1, g);
+	f = W[b1][k2]+k3; g = G[2][b1][k2]+k3;	PEQ (f,  k2, g);
+	f = W[b1][k2]+k3; g = G[3][b1][k2]+k3;	PEQ (f,  k3, g);
+	
+	f = W[k1][b2]+k3; g = G[1][k1][b2]+k3;	PEQ (f,  k1, g);
+	f = W[k1][b2]+k3; g = G[2][k1][b2]+k3;	PEQ (f, -k2, g);
+	f = W[k1][b2]+k3; g = G[3][k1][b2]+k3;	PEQ (f,  k3, g);
 
-	PEQ (W[b1][k2]+k3, -k1, G[1][b1][k2]+k3);
-	PEQ (W[b1][k2]+k3,  k2, G[2][b1][k2]+k3);
-	PEQ (W[b1][k2]+k3,  k3, G[3][b1][k2]+k3);
+	f = W[b1][b2]+k3; g = G[1][b1][b2]+k3;	PEQ (f, -k1, g);
+	f = W[b1][b2]+k3; g = G[2][b1][b2]+k3;	PEQ (f, -k2, g);
+	f = W[b1][b2]+k3; g = G[3][b1][b2]+k3;	PEQ (f,  k3, g);
 
-	PEQ (W[k1][b2]+k3,  k1, G[1][k1][b2]+k3);
-	PEQ (W[k1][b2]+k3, -k2, G[2][k1][b2]+k3);
-	PEQ (W[k1][b2]+k3,  k3, G[3][k1][b2]+k3);
-
-	PEQ (W[b1][b2]+k3, -k1, G[1][b1][b2]+k3);
-	PEQ (W[b1][b2]+k3, -k2, G[2][b1][b2]+k3);
-	PEQ (W[b1][b2]+k3,  k3, G[3][b1][b2]+k3);
       }
     }
   }
@@ -262,49 +275,56 @@ void  project (CVF G,  CF W,  const int* Dim)
   /* -- Make Gj = Gj - kj * W / (k*k) */
 
   for (k1 = 1; k1 < K; k1++) {
-    b1 = N - k1;
-
+    b1    = N - k1;
     kSqrd = SQR (k1);
+    c1    = k1 / kSqrd;
 
-    NEQ (G[1][k1][ 0]+ 0, k1/kSqrd, W[k1][ 0]+ 0);
-    NEQ (G[2][ 0][k1]+ 0, k1/kSqrd, W[ 0][k1]+ 0);
-    NEQ (G[3][ 0][ 0]+k1, k1/kSqrd, W[ 0][ 0]+k1);
+    f = G[1][k1][ 0];    g = W[k1][ 0];    NEQ (f, c1, g);
+    f = G[2][ 0][k1];    g = W[ 0][k1];    NEQ (f, c1, g);
+    f = G[3][ 0][ 0]+k1; g = W[ 0][ 0]+k1; NEQ (f, c1, g);
 
     for (k2 = 1; k2 < K && k1+k2 <= FOURKon3; k2++) {
-      b2 = N - k2;
+      b2    = N - k2;
       kSqrd = SQR (k1) + SQR (k2);
+      c1    = k1/kSqrd;
+      c2    = k2/kSqrd;
+      c3    = -c1;
 
-      NEQ (G[1][k1][k2]+ 0,  k1/kSqrd, W[k1][k2]+ 0); /* kk0 face. */
-      NEQ (G[2][k1][k2]+ 0,  k2/kSqrd, W[k1][k2]+ 0);
-      NEQ (G[1][b1][k2]+ 0, -k1/kSqrd, W[b1][k2]+ 0); /* bk0 face. */
-      NEQ (G[2][b1][k2]+ 0,  k2/kSqrd, W[b1][k2]+ 0);
-      NEQ (G[1][k1][ 0]+k2,  k1/kSqrd, W[k1][ 0]+k2); /* k0k face. */
-      NEQ (G[3][k1][ 0]+k2,  k2/kSqrd, W[k1][ 0]+k2);
-      NEQ (G[1][b1][ 0]+k2, -k1/kSqrd, W[b1][ 0]+k2); /* b0k face. */
-      NEQ (G[3][b1][ 0]+k2,  k2/kSqrd, W[b1][ 0]+k2);
-      NEQ (G[2][ 0][k1]+k2,  k1/kSqrd, W[ 0][k1]+k2); /* 0kk face. */
-      NEQ (G[3][ 0][k1]+k2,  k2/kSqrd, W[ 0][k1]+k2);
-      NEQ (G[2][ 0][b1]+k2, -k1/kSqrd, W[ 0][b1]+k2); /* 0bk face. */
-      NEQ (G[3][ 0][b1]+k2,  k2/kSqrd, W[ 0][b1]+k2);
-      
+      f = G[1][k1][k2];    g = W[k1][k2];    NEQ (f, c1, g);
+      f = G[2][k1][k2];    g = W[k1][k2];    NEQ (f, c2, g);
+      f = G[1][b1][k2];    g = W[b1][k2];    NEQ (f, c3, g);
+      f = G[2][b1][k2];    g = W[b1][k2];    NEQ (f, c2, g);
+      f = G[1][k1][ 0]+k2; g = W[k1][ 0]+k2; NEQ (f, c1, g);
+      f = G[3][k1][ 0]+k2; g = W[k1][ 0]+k2; NEQ (f, c2, g);
+      f = G[1][b1][ 0]+k2; g = W[b1][ 0]+k2; NEQ (f, c3, g);
+      f = G[3][b1][ 0]+k2; g = W[b1][ 0]+k2; NEQ (f, c2, g);
+      f = G[2][ 0][k1]+k2; g = W[ 0][k1]+k2; NEQ (f, c1, g);
+      f = G[3][ 0][k1]+k2; g = W[ 0][k1]+k2; NEQ (f, c2, g);
+      f = G[2][ 0][b1]+k2; g = W[ 0][b1]+k2; NEQ (f, c3, g);
+      f = G[3][ 0][b1]+k2; g = W[ 0][b1]+k2; NEQ (f, c2, g);
+
       for (k3 = 1; k3 < K && k2+k3 <= FOURKon3 && k1+k3 <= FOURKon3; k3++) {
 	kSqrd = SQR (k1) + SQR (k2) + SQR (k3);
+	c1    = k1/kSqrd;
+	c2    = k2/kSqrd;
+	c3    = k3/kSqrd;
 
-	NEQ (G[1][k1][k2]+k3,  k1/kSqrd, W[k1][k2]+k3);
-	NEQ (G[2][k1][k2]+k3,  k2/kSqrd, W[k1][k2]+k3);
-	NEQ (G[3][k1][k2]+k3,  k3/kSqrd, W[k1][k2]+k3);
+	f = G[1][k1][k2]+k3; g = W[k1][k2]+k3; NEQ (f,  c1, g);
+	f = G[2][k1][k2]+k3; g = W[k1][k2]+k3; NEQ (f,  c2, g);
+	f = G[3][k1][k2]+k3; g = W[k1][k2]+k3; NEQ (f,  c3, g);
 
-	NEQ (G[1][b1][k2]+k3, -k1/kSqrd, W[b1][k2]+k3);
-	NEQ (G[2][b1][k2]+k3,  k2/kSqrd, W[b1][k2]+k3);
-	NEQ (G[3][b1][k2]+k3,  k3/kSqrd, W[b1][k2]+k3);
+	f = G[1][b1][k2]+k3; g = W[b1][k2]+k3; NEQ (f, -c1, g);
+	f = G[2][b1][k2]+k3; g = W[b1][k2]+k3; NEQ (f,  c2, g);
+	f = G[3][b1][k2]+k3; g = W[b1][k2]+k3; NEQ (f,  c3, g);
+	
+	f = G[1][k1][b2]+k3; g = W[k1][b2]+k3; NEQ (f,  c1, g);
+	f = G[2][k1][b2]+k3; g = W[k1][b2]+k3; NEQ (f, -c2, g);
+	f = G[3][k1][b2]+k3; g = W[k1][b2]+k3; NEQ (f,  c3, g);
 
-	NEQ (G[1][k1][b2]+k3,  k1/kSqrd, W[k1][b2]+k3);
-	NEQ (G[2][k1][b2]+k3, -k2/kSqrd, W[k1][b2]+k3);
-	NEQ (G[3][k1][b2]+k3,  k3/kSqrd, W[k1][b2]+k3);
+	f = G[1][b1][b2]+k3; g = W[b1][b2]+k3; NEQ (f, -c1, g);
+	f = G[2][b1][b2]+k3; g = W[b1][b2]+k3; NEQ (f, -c2, g);
+	f = G[3][b1][b2]+k3; g = W[b1][b2]+k3; NEQ (f,  c3, g);
 
-	NEQ (G[1][b1][b2]+k3, -k1/kSqrd, W[b1][b2]+k3);
-	NEQ (G[2][b1][b2]+k3, -k2/kSqrd, W[b1][b2]+k3);
-	NEQ (G[3][b1][b2]+k3,  k3/kSqrd, W[b1][b2]+k3);
       }
     }
   }
