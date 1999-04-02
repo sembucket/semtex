e79e5cef8423ff7c9fd271aaf12c946b120f48c0
hmb <>
1999-04-02 04:15:53 +0000

    Added statement about GNU public license

--- semtex/dns/drive.C
+++ semtex/dns/drive.C
@@ -4,6 +4,21 @@
 // SYNOPSIS:
 // --------
 // Control spectral element DNS for incompressible flows.
+// Copyright (C) 1994, 1999  Hugh Blackburn.
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
 // USAGE:
 // -----
@@ -21,7 +36,7 @@
 // P.O. Box 56
 // Highett, Vic 3190
 // Australia
-// hmb@dbce.csiro.au
+// hugh.blackburn@dbce.csiro.au
 //////////////////////////////////////////////////////////////////////////////
 
 static char
