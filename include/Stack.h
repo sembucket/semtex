ae23cf3effeda070d2276ec3f29dc928a6d7633c
hmb <>
1999-05-27 07:22:29 +0000

    Copied in an alternative definition which has a "depth" operator.

--- include/Stack.h
+++ include/Stack.h
@@ -3,59 +3,74 @@
 ///////////////////////////////////////////////////////////////////////////////
 // Stack.h: templated operations for LIFO stack.
 //
-// Summary:
-//   creator, destructor, push, pop, isEmpty.
+// Summary: creator, destructor, push, pop, depth.
 //
 // $Id$
 ///////////////////////////////////////////////////////////////////////////////
 
 
-template<class T> class StackNode {
+template<class T>
+class Stack {
 public:
-  StackNode (T x) : next (0), datum (x) {}
-  StackNode<T>* next;
-  T     datum;
-};
+  Stack () : top (0), stack_depth (0) {}
+ ~Stack ();
 
-template<class T> class Stack {
+  void  push    (T x);
+  T     pop     ();
+  int   depth   () const { return stack_depth; }
+
 private:
-  StackNode<T>* top;
+  class Node {
+  public:
+    Node (T x) : next (0), datum (x) {}
+    Node* next;
+    T     datum;
+  };
+
+  Node* top;
+  int   stack_depth;
 
   Stack(const Stack<T>&);                // Prohibit, since not implemented.
   Stack<T>& operator=(const Stack<T>&);  // Prohibit, since not implemented.
-
-public:
-  int   isEmpty () { return top == 0; }
-
-  Stack  () : top (0) { }
-  ~Stack (){ while (!isEmpty()) {
-    StackNode<T>* p = top->next;
-    delete top;
-    top = p;} 
-  }
-
-  void  push    (T x) {
-    if (isEmpty())
-      top = new StackNode<T> (x);
-    else {
-      StackNode<T>* p = new StackNode<T> (x);
-      p -> next = top;
-      top = p;
-    }
-  }
-
-  T     pop     () {
-    if (isEmpty()) {
-      return 0;
-    } else {
-      StackNode<T>* p     = top;
-      T             value = top -> datum;
-      top = top -> next;
-      delete p;
-      return value;
-    }
-  }
-
 };
+
+
+template<class T>
+inline void Stack<T>::push(T x) {
+  if (stack_depth) {
+    Node* p   = new Node (x);
+    p -> next = top;
+    top       = p;
+  } else {
+    top = new Node (x);
+  }
+  stack_depth++;
+}
+
+
+template<class T>
+inline T Stack<T>::pop() {
+  if (stack_depth) {
+    Node* p     = top;
+    T     value = top -> datum;
+    top         = top -> next;
+    delete p;
+    stack_depth--;
+    return value;
+  } else {
+    return 0;
+  }
+
+}
+
+
+template<class T>
+inline Stack<T>::~Stack() {
+  while (stack_depth--) {
+    Node* p = top -> next;
+    delete top;
+    top = p;
+  }
+}
 
 #endif
