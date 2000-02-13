6b29ef568adcb5bb640ce81b9e27e95260b0a231
hmb <>
2000-02-13 23:56:17 +0000

    Utility to calculate TKE in given Fourier mode.

---
+++ semtex/utility/moden.c
@@ -0,0 +1,208 @@
+/*****************************************************************************
+ * moden.c: from a 3D data file, compute 2D distribution of kinetic
+ * energy in named mode, output 2D data file.  Field must be binary
+ * format.  By default, output energy in mode 0.
+ *
+ * Copyright (c) 1999 Hugh Blackburn
+ *
+ * USAGE
+ * -----
+ * moden [-h] [-m mode] [input[.fld]
+ *
+ * $Id$
+ *****************************************************************************/
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+#include <femlib.h>
+#include <femdef.h>
+#include <alplib.h>
+
+static void getargs (int, char**, FILE**, int*);
+static int  _index  (const char*, char);
+
+static char prog[] = "moden";
+static const char *hdr_fmt[] = { 
+  "%-25s "              "Session\n",
+  "%-25s "              "Created\n",
+  "%-5d%-5d%-5d%-10d "  "Nr, Ns, Nz, Elements\n",
+  "%-25d "              "Step\n",
+  "%-25.6g "            "Time\n",
+  "%-25.6g "            "Time step\n",
+  "%-25.6g "            "Kinvis\n",
+  "%-25.6g "            "Beta\n",
+  "%-25s "              "Fields written\n",
+  "%-25s "              "Format\n"
+};
+
+
+int main (int    argc,
+	  char** argv)
+/* ------------------------------------------------------------------------- *
+ * Wrapper.
+ * ------------------------------------------------------------------------- */
+{
+  char   buf[STR_MAX], fields[STR_MAX], fmt[STR_MAX];
+  int    i, n, np, nz, nel, mode = 0, swab = 0;
+  int    nfields, nplane, npts, npad, ntot;
+  FILE   *fp_in = stdin, *fp_out = stdout;
+  double **data, *plane, *vcmpt;
+
+  getargs (argc, argv, &fp_in, &mode);
+  format  (fmt);
+
+  while (fgets (buf, STR_MAX, fp_in)) { 
+
+    fputs (buf, fp_out); fgets (buf, STR_MAX, fp_in);
+    fputs (buf, fp_out); fgets (buf, STR_MAX, fp_in);
+    
+    if (sscanf (buf, "%d%*s%d%d", &np, &nz, &nel) != 3)
+      message (prog, "unable to read the file size", ERROR);
+
+    if (2 * mode > nz) {
+      sprintf (fields, "too many modes (%1d) for input (nz = %1d)", mode, nz);
+      message (prog, fields, ERROR);
+    }
+    fprintf (fp_out, hdr_fmt[2], np, np, 1, nel);
+
+    n = 6;
+    while (--n) {
+      fgets (buf, STR_MAX, fp_in);
+      fputs (buf, fp_out);   
+    }
+
+
+    fgets(fields, STR_MAX, fp_in);
+    for (nfields = 0, i = 0; i < 25; i++) if (isalpha(fields[i])) nfields++;
+    if (!(strchr(fields, 'u') && strchr(fields, 'v') && strchr(fields, 'w')))
+	message (prog, "need fields u, v, w to compute K.E.", ERROR);
+    fprintf (fp_out, hdr_fmt[8], "q");
+
+    fgets (buf, STR_MAX, fp_in);
+    for (i = 0; i < strlen (buf); i++) buf[i] = tolower (buf[i]);
+
+    if (!strstr(buf, "binary"))
+      message (prog, "input file not binary format", ERROR);
+    if (!strstr (buf, "endian"))
+      message (prog, "input field file in unknown binary format", WARNING);
+    else
+      swab = ((strstr (buf, "big") && strstr (fmt, "little")) ||
+	      (strstr (fmt, "big") && strstr (buf, "little")) );
+    sprintf (buf, "%s %s", "binary", fmt);
+    fprintf (fp_out, hdr_fmt[9], buf);
+
+    // -- Set sizes, allocate storage.
+
+    nplane = np * np * nel;
+    npts   = nz * nplane;
+    ntot   = nfields * nplane;
+
+    data   = dmatrix (0, nfields - 1, 0, npts - 1);
+    plane  = dvector (0, nplane - 1);
+    
+    // -- Read in all data fields.
+
+    for (i = 0; i < nfields; i++) {
+      if (fread (data[i], sizeof (double), npts, fp_in) != npts)
+	message (prog, "an error has occured while reading", ERROR);
+      if (swab) dbrev (npts, data[i], 1, data[i], 1);
+      dDFTr (data[i], nz, nplane, +1);
+    }
+    
+    // -- Compute K.E.: start by adding in real part.
+
+    vcmpt = data[_index (fields, 'u')] + 2 * mode * nplane;
+    dvmul (nplane, vcmpt, 1, vcmpt, 1, plane, 1);
+    vcmpt = data[_index (fields, 'v')] + 2 * mode * nplane;
+    dvvtvp (nplane, vcmpt, 1, vcmpt, 1, plane, 1, plane, 1);
+    vcmpt = data[_index (fields, 'w')] + 2 * mode * nplane;
+    dvvtvp (nplane, vcmpt, 1, vcmpt, 1, plane, 1, plane, 1);
+
+    // -- Add in imaginary part for if not mode zero.
+
+    if (mode) {
+      vcmpt = data[_index (fields, 'u')] + (2 * mode + 1) * nplane;
+      dvvtvp (nplane, vcmpt, 1, vcmpt, 1, plane, 1, plane, 1);
+      vcmpt = data[_index (fields, 'v')] + (2 * mode + 1) * nplane;
+      dvvtvp (nplane, vcmpt, 1, vcmpt, 1, plane, 1, plane, 1);
+      vcmpt = data[_index (fields, 'w')] + (2 * mode + 1) * nplane;
+      dvvtvp (nplane, vcmpt, 1, vcmpt, 1, plane, 1, plane, 1);
+    }
+
+    // -- Normalise to make q = 0.5*UiUi.
+
+    dsmul (nplane, 0.5, plane, 1, plane, 1);
+    
+    if (fwrite (plane, sizeof (double), nplane, fp_out) != nplane)
+      message (prog, "an error has occured while writing", ERROR);
+
+    freeDmatrix (data,  0, 0);
+    freeDvector (plane, 0);
+  } 
+  
+  return EXIT_SUCCESS;
+}
+
+
+static void getargs (int     argc ,
+		     char**  argv ,
+		     FILE**  fp_in,
+		     int*    mode )
+/* ------------------------------------------------------------------------- *
+ * Parse command line arguments.
+ * ------------------------------------------------------------------------- */
+{
+  char c;
+  int  i;
+  char fname[FILENAME_MAX];
+  char usage[] = "moden [-h] [-m mode] [input[.fld]\n";
+
+  while (--argc && (*++argv)[0] == '-')
+    while (c = *++argv[0])
+      switch (c) {
+      case 'h':
+	fputs (usage, stderr);
+	exit  (EXIT_SUCCESS);
+	break;
+      case 'm':
+	if (*++argv[0])
+	  *mode = atoi (*argv);
+	else {
+	  *mode = atoi (*++argv);
+	  argc--;
+	}
+	break;
+      default:
+	fprintf (stderr, "%s: unknown option -- %c\n", prog, c);
+	break;
+      }
+
+  if (argc == 1)
+    if ((*fp_in = fopen(*argv, "r")) == (FILE*) NULL) {
+      sprintf(fname, "%s.fld", *argv);
+      if ((*fp_in = fopen(fname, "r")) == (FILE*) NULL) {
+	fprintf(stderr, "%s: unable to open input file -- %s or %s\n",
+		prog, *argv, fname);
+	exit (EXIT_FAILURE);
+      }
+    }
+}
+
+
+static int _index (const char* s,
+		   char        c)
+/* ------------------------------------------------------------------------- *
+ * Return index of c in s, -1 if not found.
+ * ------------------------------------------------------------------------- */
+{
+  int       i;
+  const int len = strlen (s);
+
+  for (i = 0; i < len; i++) if (s[i] == c) return i;
+
+  return -1;
+}
