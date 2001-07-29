a9b9a38aa27a2a4669b8d613ed991f8d70e0b75f
hmb <>
2001-07-29 22:00:12 +0000

    added dpt.C lagint.C lagpro.C to femlib/tests

---
+++ semtex/femlib/tests/dpt.C
@@ -0,0 +1,126 @@
+//////////////////////////////////////////////////////////////////////////////
+// dpt.C: carry out 1D forward/inverse discrete polynomial transforms.
+//
+// Usage: dpt -<type> [-i] [file]
+// where
+// -i       ... specifies inverse transform
+// -<type>  ... shape function kind
+//              type == l ==> Legendre polynomials
+//              type == m ==> 'Modal'  polynomials
+//
+// Forward transform: N values are input from file; these are assumed
+// to be nodal values at the GLL points on [-1, 1].  Compute the 1D DPT
+// and print up the spectral coefficients.
+// 
+// Inverse transform: the N values are assumed to be spectral
+// coefficients; compute the nodal values.
+//
+// $Id$
+//////////////////////////////////////////////////////////////////////////////
+
+#include <stdlib.h>
+#include <math.h>
+
+#include <iostream.h>
+#include <iomanip.h>
+#include <fstream.h>
+
+#include <femdef.h>
+#include <Array.h>
+#include <Veclib.h>
+#include <Femlib.h>
+#include <Blas.h>
+#include <Utility.h>
+#include <Stack.h>
+
+static void getargs (int       argc,
+		     char**    argv,
+		     int&      invt,
+		     char&     ptyp,
+		     ifstream& file)
+// ---------------------------------------------------------------------------
+// Parse command-line args.
+// ---------------------------------------------------------------------------
+{
+  const char *usage = 
+    "Usage: dpt -<type> [-i] [file]\n"
+    "where\n"
+    "-i       ... specifies inverse transform\n"
+    "-<type>  ... shape function kind\n"
+    "             type == l ==> Legendre polynomials\n"
+    "             type == m ==> 'Modal'  polynomials\n";
+
+  while (--argc  && **++argv == '-')
+    switch (*++argv[0]) {
+    case 'i':
+      invt = INVERSE;
+      break;
+    default:
+      ptyp = *argv[0];
+      break;
+    }
+ 
+  if (!(ptyp == 'l' || ptyp == 'm')) { cerr << usage; exit (EXIT_FAILURE); }
+
+  if   (argc == 1) file.open   (*argv, ios::in);
+  else             file.attach (0);
+
+  if (!file) {
+    cerr << "dpt: unable to open input file" << endl; exit (EXIT_FAILURE);
+  }
+}
+
+
+int loadVals (ifstream&       file,
+	      vector<double>& val )
+// ---------------------------------------------------------------------------
+// Get nodal values from file, return number of values.
+// ---------------------------------------------------------------------------
+{
+  int           ntot, num = 0;
+  double        datum;
+  Stack<double> data;
+
+  while (file >> datum) { data.push (datum); num++; }
+
+  val.setSize (ntot = num);
+
+  while (num--) val[num] = data.pop();
+
+  return ntot;
+}
+
+
+int main (int    argc,
+	  char** argv)
+// ---------------------------------------------------------------------------
+// Driver.
+// ---------------------------------------------------------------------------
+{
+  ifstream       file;
+  int            i, ngll, dir = FORWARD;
+  char           polytype;
+  vector<double> u, v;
+  const double   *F, *I;
+
+  cout.precision (8);
+  cout.setf (ios::fixed, ios::floatfield);
+
+  getargs (argc, argv, dir, polytype, file);
+
+  v.setSize (ngll = loadVals (file, u));
+
+  switch (polytype) {
+  case 'l': Femlib::legTran (ngll, &F, 0, &I, 0, 0, 0); break;
+  case 'm': Femlib::modTran (ngll, &F, 0, &I, 0, 0, 0); break;
+  }
+
+  switch (dir) {
+  case FORWARD: Blas::mxv (F, ngll, u(), ngll, v()); break;
+  case INVERSE: Blas::mxv (I, ngll, u(), ngll, v()); break;
+  }
+
+  for (i = 0; i < ngll; i++) cout << i << '\t' << v[i] << endl;
+
+  return EXIT_SUCCESS;
+}
---
+++ semtex/femlib/tests/lagint.C
@@ -0,0 +1,121 @@
+//////////////////////////////////////////////////////////////////////////////
+// lagint.C: given a table of N values, assumed located at the GLL
+// points in [-1, 1], return I values of the N-1 polynomial
+// interpolant for locations uniformly distributed on the interval,
+// computed using the Lagrange polynomials.
+//
+// Usage: lagint -i <num> [file]
+// where
+// -i <num> ... number of interpolant points on [-1, 1]
+//
+// $Id$
+//////////////////////////////////////////////////////////////////////////////
+
+#include <stdlib.h>
+#include <math.h>
+
+#include <iostream.h>
+#include <iomanip.h>
+#include <fstream.h>
+
+#include <femdef.h>
+#include <Array.h>
+#include <Veclib.h>
+#include <Femlib.h>
+#include <Blas.h>
+#include <Utility.h>
+#include <Stack.h>
+
+
+void getargs (int       argc,
+	      char**    argv,
+	      int&      nint,
+	      ifstream& file)
+// ---------------------------------------------------------------------------
+// Parse command-line args.
+// ---------------------------------------------------------------------------
+{
+  const char *usage = 
+    "Usage: lagint -i <num> [file]\n"
+    "where\n"
+    "-i <num> ... number of interpolant points on [-1, 1]\n";
+
+  while (--argc  && **++argv == '-')
+    switch (*++argv[0]) {
+    case 'i':
+      if (*++argv[0]) nint = atoi (*argv);
+      else { --argc;  nint = atoi (*++argv); }
+      break;
+    default:
+      cerr << usage; exit (EXIT_FAILURE);
+      break;
+    }
+
+  if (!nint) { cerr << usage; exit (EXIT_FAILURE); }
+    
+  if   (argc == 1) file.open   (*argv, ios::in);
+  else             file.attach (0);
+
+  if (!file) {
+    cerr << "lagint: unable to open input file" << endl; exit (EXIT_FAILURE);
+  }
+}
+
+
+int loadVals (ifstream&       file,
+	      vector<double>& val )
+// ---------------------------------------------------------------------------
+// Get nodal values from file, return number of values.
+// ---------------------------------------------------------------------------
+{
+  int           ntot, num = 0;
+  double        datum;
+  Stack<double> data;
+
+  while (file >> datum) { data.push (datum); num++; }
+
+  val.setSize (ntot = num);
+
+  while (num--) val[num] = data.pop();
+
+  return ntot;
+}
+
+
+int main (int    argc,
+	  char** argv)
+// ---------------------------------------------------------------------------
+// Driver.
+// ---------------------------------------------------------------------------
+{
+  ifstream       file;
+  int            i, j, ngll, nint = 0;
+  vector<double> u, v, w, x, z;
+  double         **II, **IT;
+
+  cout.precision (8);
+  cout.setf (ios::fixed, ios::floatfield);
+
+  getargs (argc, argv, nint, file);
+
+  ngll = loadVals (file, v);
+  z.setSize (ngll);
+  w.setSize (ngll);
+
+  x.setSize (nint);
+  u.setSize (nint);
+
+  for (i = 0; i < nint; i++) x[i] = -1.0 + i * 2.0/(nint - 1);
+
+  II = dmatrix (0, nint-1, 0, ngll-1);
+  IT = dmatrix (0, ngll-1, 0, nint-1);
+
+  Femlib::GLLzw       (ngll, z(), w());
+  Femlib::LagrangeInt (ngll, z(), nint, x(), II, IT);
+
+  Blas::mxv (*II, nint, v(), ngll, u());
+
+  for (i = 0; i < nint; i++) cout << x[i] << '\t' << u[i] << endl;
+
+  return EXIT_SUCCESS;
+}
