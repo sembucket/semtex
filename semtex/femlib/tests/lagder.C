edf6e1748b81b6f98b8cf57205424b9eccfd45ae
hmb <>
2001-08-28 01:56:40 +0000

    added clipup and clipdn to alplib, added lagder to femlib tests

--- semtex/femlib/tests/lagder.C
+++ semtex/femlib/tests/lagder.C
@@ -1,13 +1,8 @@
 //////////////////////////////////////////////////////////////////////////////
-// lagpro.C: given a table of N values, assumed located at the GLL
-// points in [-1, 1], return values of the original interpolating
-// polynomial, order N-1, at I GLL points; also return the values of
-// the order I-1 interpolating polynomial for the new I GLL points
-// evaluated at the original N GLL points.
+// lagder.C: given a table of N values, assumed located at the GLL
+// points in [-1, 1], return values of the derivative at the same points.
 //
-// Usage: lagpro -i <num> [file]
-// where
-// -i <num> ... number of new GLL points on [-1, 1]
+// Usage: lagder [file]
 //
 // $Id$
 //////////////////////////////////////////////////////////////////////////////
@@ -30,35 +25,26 @@
 
 void getargs (int       argc,
 	      char**    argv,
-	      int&      nint,
 	      ifstream& file)
 // ---------------------------------------------------------------------------
 // Parse command-line args.
 // ---------------------------------------------------------------------------
 {
   const char *usage = 
-    "Usage: lagpro -i <num> [file]\n"
-    "where\n"
-    "-i <num> ... number of new GLL points on [-1, 1]\n";
+    "Usage: lagder [file]\n";
 
   while (--argc  && **++argv == '-')
     switch (*++argv[0]) {
-    case 'i':
-      if (*++argv[0]) nint = atoi (*argv);
-      else { --argc;  nint = atoi (*++argv); }
-      break;
     default:
       cerr << usage; exit (EXIT_FAILURE);
       break;
     }
 
-  if (!nint) { cerr << usage; exit (EXIT_FAILURE); }
-    
   if   (argc == 1) file.open   (*argv, ios::in);
   else             file.attach (0);
 
   if (!file) {
-    cerr << "lagint: unable to open input file" << endl; exit (EXIT_FAILURE);
+    cerr << "lagder: unable to open input file" << endl; exit (EXIT_FAILURE);
   }
 }
 
@@ -90,31 +76,23 @@ int main (int    argc,
 // ---------------------------------------------------------------------------
 {
   ifstream       file;
-  int            i, nold, nnew = 0;
+  int            i, np;
   vector<double> u, v;
-  const double   **IF, **IB;
+  const double   **DV;
 
   cout.precision (8);
   cout.setf (ios::fixed, ios::floatfield);
 
-  getargs (argc, argv, nnew, file);
+  getargs (argc, argv, file);
 
-  nold = loadVals (file, v);
+  np = loadVals (file, v);
 
-  u.setSize (nnew);
+  u.setSize (np);
 
-  if (nold == nnew)
-    Veclib::copy (nold, v(), 1, u(), 1);
-  else {
-    Femlib::mesh (GLL, GLL, nold, nnew, 0, &IF, 0, 0, 0);
-    Femlib::mesh (GLL, GLL, nnew, nold, 0, &IB, 0, 0, 0);
-    Blas::mxv    (*IF, nnew, v(), nold, u());
-    Blas::mxv    (*IB, nold, u(), nnew, v());
-  }
+  Femlib::mesh (GLL, GLL, np, np, 0, 0, 0, &DV, 0);
+  Blas::mxv    (*DV, np, v(), np, u());
 
-  for (i = 0; i < nnew; i++) cout << i << '\t' << u[i] << endl;
-  cout << endl;
-  for (i = 0; i < nold; i++) cout << i << '\t' << v[i] << endl;
+  for (i = 0; i < np; i++) cout << i << '\t' << u[i] << endl;
 
   return EXIT_SUCCESS;
 }
