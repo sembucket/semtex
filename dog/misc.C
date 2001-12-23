de245541cbd659dd2be5efd2a202f971e2166137
hmb <>
2001-12-23 03:15:42 +0000

    These are modified versions for stability analysis.

---
+++ dog/misc.C
@@ -0,0 +1,240 @@
+///////////////////////////////////////////////////////////////////////////////
+// misc.C: miscellaneous routines for I/O, memory management, service
+// routines that don't fit class structures.
+//
+// Copyright (C) 1994, 2001 Hugh BLackburn
+//
+// $Id$
+///////////////////////////////////////////////////////////////////////////////
+
+#include <Sem.h>
+#include <time.h>
+
+
+ostream& printVector (ostream&      strm,
+		      const char*   fmt , 
+		      const integer ntot,
+		                    ... )
+// ---------------------------------------------------------------------------
+// Print up a variable number of numeric vectors on strm, in columns.
+//
+// The format specifier is gives the number and type of the vectors, with
+// type specified by the first character.  The vectors must all be of the
+// same type & length.
+//
+// Allowed types are: int ("i"), real ("r").
+// Examples: four integer vectors ==> fmt is "iiii".  Two reals ==> "rr".
+// 
+// Vectors are printed in a fixed field width of 15, regardless of type.
+// ---------------------------------------------------------------------------
+{
+  char    routine[] = "printVector";
+  integer nvect;
+  va_list ap;
+
+  nvect = strlen (fmt);
+  if (! nvect   ) message (routine, "empty format string",   ERROR  );
+  if (nvect > 10) message (routine, "more than 10 vectors?", WARNING);
+  if (ntot  <  0) message (routine, "ntot < 0",              ERROR  );
+
+  switch (fmt[0]) {
+
+  case 'i': {
+    integer** u = new integer* [nvect];
+    va_start (ap, ntot);
+    for (integer k = 0; k < nvect; k++) u[k] = va_arg (ap, integer*);
+    va_end (ap);
+    for (register integer l = 0; l < ntot; l++) {
+      for (register integer j = 0; j < nvect; j++)
+	strm << setw(15) << u[j][l];
+      strm << endl;
+    }
+    delete [] u;
+    break;
+  }
+  case 'r': {
+    real** u = new real* [nvect];
+    va_start (ap, ntot);
+    for (integer k = 0; k < nvect; k++) u[k] = va_arg (ap, real*);
+    va_end (ap);
+    for (register integer l = 0; l < ntot; l++) {
+      for (register integer j = 0; j < nvect; j++)
+	strm << setw(15) << u[j][l];
+      strm << endl;
+    }
+    delete [] u;
+    break;
+  }
+  default:
+    message (routine, fmt, ERROR);
+    break;
+  }
+
+  if (!strm) message (routine, "output failed", ERROR);
+
+  return strm;
+}
+
+
+char* upperCase (char *s)
+// ---------------------------------------------------------------------------
+// Uppercase characters in string.
+// ---------------------------------------------------------------------------
+{
+  char *z(s); while (*z = toupper (*z)) z++; return s;
+}
+
+
+void writeField (ofstream&          file   ,
+		 const char*        session,
+		 const int          runstep,
+		 const real         runtime,
+		 vector<AuxField*>& field  )
+// ---------------------------------------------------------------------------
+// Write fields out to an opened file, binary prism format.  Output is
+// only done by the root processor.
+// ---------------------------------------------------------------------------
+{
+  const char routine [] = "writeField";
+  const char *hdr_fmt[] = { 
+    "%-25s "    "Session\n",
+    "%-25s "    "Created\n",
+    "%-25s "    "Nr, Ns, Nz, Elements\n",
+    "%-25d "    "Step\n",
+    "%-25.6g "  "Time\n",
+    "%-25.6g "  "Time step\n",
+    "%-25.6g "  "Kinvis\n",
+    "%-25.6g "  "Beta\n",
+    "%-25s "    "Fields written\n",
+    "%-25s "    "Format\n"
+  };
+
+  char      s1[StrMax], s2[StrMax];
+  time_t    tp (time (0));
+  int       i;
+  const int N = field.getSize();
+
+  if (N < 1) return;
+
+  ROOTONLY {
+    sprintf (s1, hdr_fmt[0], session);
+    file << s1;
+
+    strftime (s2, 25, "%a %b %d %H:%M:%S %Y", localtime (&tp));
+    sprintf  (s1, hdr_fmt[1], s2);
+    file << s1;
+
+    field[0] -> describe (s2);
+    sprintf (s1, hdr_fmt[2], s2);
+    file << s1;
+
+    sprintf (s1, hdr_fmt[3], runstep);
+    file << s1;
+
+    sprintf (s1, hdr_fmt[4], runtime);
+    file << s1;
+
+    sprintf (s1, hdr_fmt[5], Femlib::value ("D_T"));
+    file << s1;
+
+    sprintf (s1, hdr_fmt[6], Femlib::value ("KINVIS"));
+    file << s1;
+
+    sprintf (s1, hdr_fmt[7], Femlib::value ("BETA"));
+    file << s1;
+
+    for (i = 0; i < N; i++) s2[i] = field[i] -> name();
+    s2[i] = '\0';
+    sprintf (s1, hdr_fmt[8], s2);
+    file << s1;
+
+    sprintf (s2, "binary ");
+    Veclib::describeFormat (s2 + strlen (s2));
+    sprintf (s1, hdr_fmt[9], s2);
+    file << s1;
+  }
+
+  for (i = 0; i < N; i++) file << *field[i];
+
+  ROOTONLY {
+    if (!file) message (routine, "failed writing field file", ERROR);
+    file << flush;
+  }
+}
+
+
+Header::Header ()
+// ---------------------------------------------------------------------------
+// Allocate storage for Header.
+// ---------------------------------------------------------------------------
+{
+  sess = new char [StrMax];
+  sesd = new char [StrMax];
+  flds = new char [StrMax];
+  frmt = new char [StrMax];
+
+  sess[0] = sesd[0] = flds[0] = '\0';
+  sprintf (frmt, "binary "); Veclib::describeFormat (frmt + strlen (frmt));
+  nr = ns = nz = nel = step = 0;
+  time = dt = visc = beta = 0.0;
+}
+
+
+ifstream& operator >> (ifstream& file,
+		       Header&   hdr )
+// ---------------------------------------------------------------------------
+// Insert data into Header struct from file.
+// ---------------------------------------------------------------------------
+{
+  file.getline(hdr.sess, 25)             .ignore(StrMax, '\n');
+  file.getline(hdr.sesd, 25)             .ignore(StrMax, '\n');
+  file >> hdr.nr >> hdr.ns >> hdr.nz >> hdr.nel; file.ignore(StrMax, '\n');
+  file >> hdr.step;                  file.ignore(StrMax, '\n');
+  file >> hdr.time;                  file.ignore(StrMax, '\n');
+  file >> hdr.dt;                    file.ignore(StrMax, '\n');
+  file >> hdr.visc;                  file.ignore(StrMax, '\n');
+  file >> hdr.beta;                  file.ignore(StrMax, '\n');
+  file >> hdr.flds;                  file.ignore(StrMax, '\n');
+  file.getline(hdr.frmt, 25)             .ignore(StrMax, '\n');
+}
+
+
+ofstream& operator << (ofstream& file,
+		       Header&   hdr )
+// ---------------------------------------------------------------------------
+// Put data from Header struct onto file. Use current time info.
+// ---------------------------------------------------------------------------
+{
+  const char routine [] = "file << Header";
+  const char *hdr_fmt[] = { 
+    "%-25s "                "Session\n",
+    "%-25s "                "Created\n",
+    "%-5d %-5d %-5d %-5d "  "Nr, Ns, Nz, Elements\n",
+    "%-25d "                "Step\n",
+    "%-25.6g "              "Time\n",
+    "%-25.6g "              "Time step\n",
+    "%-25.6g "              "Kinvis\n",
+    "%-25.6g "              "Beta\n",
+    "%-25s "                "Fields written\n",
+    "%-25s "                "Format\n"
+  };
+
+  char   s1[StrMax], s2[StrMax];
+  time_t tp (time (0));
+
+  strftime (s2, 25, "%a %b %d %H:%M:%S %Y", localtime (&tp));
+
+  sprintf  (s1, hdr_fmt[0], hdr.sess);                        file << s1;
+  sprintf  (s1, hdr_fmt[1], s2);                              file << s1;
+  sprintf  (s1, hdr_fmt[2], hdr.nr, hdr.ns, hdr.nz, hdr.nel); file << s1;
+  sprintf  (s1, hdr_fmt[3], hdr.step);                        file << s1;
+  sprintf  (s1, hdr_fmt[4], Femlib::value ("t"));             file << s1;
+  sprintf  (s1, hdr_fmt[5], Femlib::value ("D_T"));           file << s1;
+  sprintf  (s1, hdr_fmt[6], Femlib::value ("KINVIS"));        file << s1;
+  sprintf  (s1, hdr_fmt[7], Femlib::value ("BETA"));          file << s1;
+  sprintf  (s1, hdr_fmt[8], hdr.flds);                        file << s1;
+  sprintf  (s1, hdr_fmt[9], hdr.frmt);                        file << s1;
+
+  if (!file) message (routine, "failed writing field file header", ERROR);
+  file << flush;
+}
---
+++ floquet/statistics.C
@@ -0,0 +1,306 @@
+///////////////////////////////////////////////////////////////////////////////
+// statistics.C: routines for statistical analysis of AuxFields.
+//
+// Copyright (C) 1994, 1999 Hugh Blackburn
+//
+// At present, this is limited to running averages of primitive
+// variables and product terms for Reynolds stresses.
+//
+// Naming scheme for "Reynolds stresses":
+//
+//                      / uu uv uw \     /  A  B  D \
+//                      | .  vv vw |  =  |  .  C  E |
+//                      \ .  .  ww /     \  .  .  F /
+//
+// Reynolds stresses are selected for computation if AVERAGE > 1.  In
+// this case it is assumed that there will be at least DIM fields to
+// be multiplied together as indicated.  NB: what is computed are the
+// running average of the products uu, uv, etc, which are NOT the
+// actual Reynolds stresses: they need to have the products of the
+// mean values UU, UV etc subtracted, assumed to occur in
+// postprocessing.
+//
+// Running averages are kept semi-Fourier, Reynolds stress products in
+// physical space to minimize number of transforms needed.
+//
+// $Id$
+///////////////////////////////////////////////////////////////////////////////
+
+#include <Sem.h>
+#include <time.h>
+
+
+Statistics::Statistics (Domain*            D    ,
+			vector<AuxField*>& extra) : 
+// ---------------------------------------------------------------------------
+// Store averages for all Domain Fields, and any extra AuxFields
+// supplied.
+//
+// Try to initialize from file session.avg, failing that set all
+// buffers to zero.  Number of fields in file should be same as
+// Statistics::avg buffer.
+//
+// NR = Number of Reynolds stress averaging buffers, set if AVERAGE > 1.
+// ---------------------------------------------------------------------------
+  name (D -> name),
+  base (D)
+{
+  integer       i, j;
+  const integer ND    = Geometry::nDim();
+  const integer NF    = base -> u.getSize();
+  const integer NE    = extra.getSize();
+  const integer NR    = ((integer) Femlib::value ("AVERAGE") > 1) ? 
+                            ((ND + 1) * ND) >> 1 : 0;
+  const integer NT    = NF + NE + NR;
+  const integer nz    = Geometry::nZProc();
+  const integer ntot  = Geometry::nTotProc();
+  real*         alloc = new real [(size_t) NT * ntot];
+
+  ROOTONLY cout << "-- Initialising averaging  : ";  
+
+  // -- Set pointers, allocate storage.
+
+  src.setSize (NF + NE);	// -- Straight running average of these.
+  avg.setSize (NT);		// -- Additional are computed from src.
+  
+  for (i = 0; i < NF; i++) src[     i] = (AuxField*) base -> u[i];
+  for (i = 0; i < NE; i++) src[NF + i] = extra[i];
+
+  for (j = 0, i = 0; i < NF + NE; i++, j++)
+    avg[i] = new AuxField (alloc+j*ntot, nz, base -> elmt, src[i] -> name());
+  for (i = 0; i < NT - NF - NE; i++, j++)
+    avg[i + NF + NE] = new AuxField (alloc+j*ntot, nz, base -> elmt, 'A' + i);
+
+  // -- Initialise averages, either from file or zero.
+  //    This is much the same as Domain input routine.
+
+  char     s[StrMax];
+  ifstream file (strcat (strcpy (s, name), ".avg"));
+
+  if (file) {
+    ROOTONLY {
+      cout << "read from file " << s;
+      cout.flush();
+    }
+    file >> *this;
+    file.close();
+  
+  } else {			// -- No file, set to zero.
+    ROOTONLY cout << "set to zero";
+    for (i = 0; i < NT; i++) *avg[i] = 0.0;
+    navg = 0;
+  }
+
+  ROOTONLY cout << endl;
+}
+
+
+void Statistics::update (AuxField** work)
+// ---------------------------------------------------------------------------
+// Update running averages, using zeroth time level of work as
+// workspace.  Reynolds stress terms are calculated without
+// dealiasing, and are held in physical space.
+// ---------------------------------------------------------------------------
+{
+  integer       i;
+  const integer NT = avg.getSize();
+  const integer ND = Geometry::nDim();
+  const integer NR = ((integer) Femlib::value ("AVERAGE") > 1) ? 
+                            ((ND + 1) * ND) >> 1 : 0;
+  const integer NA = NT - NR;
+
+  if (NR) {
+    
+    // -- Running averages and Reynolds stresses.
+
+    for (i = 0; i < ND; i++) {
+      *work[i] = *src[i];
+    }
+
+    for (i = 0; i < NT; i++) *avg[i] *= (real) navg;
+
+    for (i = 0; i < NA; i++) *avg[i] += *src[i];
+
+    avg[NA + 0] -> timesPlus (*work[0], *work[0]);
+    avg[NA + 1] -> timesPlus (*work[0], *work[1]);
+    avg[NA + 2] -> timesPlus (*work[1], *work[1]);
+    
+    if (ND > 2) {
+      avg[NA + 3] -> timesPlus (*work[0], *work[2]);
+      avg[NA + 4] -> timesPlus (*work[1], *work[2]);
+      avg[NA + 5] -> timesPlus (*work[2], *work[2]);
+    }
+
+    for (i = 0; i < NT; i++) *avg[i] /= (real) (navg + 1);
+
+  } else {
+
+    // -- Running averages only.
+
+    for (i = 0; i < NA; i++) {
+      *avg[i] *= (real)  navg;
+      *avg[i] += *src[i];
+      *avg[i] /= (real) (navg + 1);
+    }
+  }
+
+  navg++;
+}
+
+
+void Statistics::dump ()
+// ---------------------------------------------------------------------------
+// Similar to Domain::dump.
+// ---------------------------------------------------------------------------
+{
+  const integer step     = base -> step;
+  const integer periodic = !(step %  (integer) Femlib::value ("IO_FLD"));
+  const integer initial  =   step == (integer) Femlib::value ("IO_FLD");
+  const integer final    =   step == (integer) Femlib::value ("N_STEP");
+
+  if (!(periodic || final)) return;
+
+  integer       i;
+  ofstream      output;
+  const integer NT = avg.getSize();
+  const integer ND = Geometry::nDim();
+  const integer NR = ((integer) Femlib::value ("AVERAGE") > 1) ? 
+                            ((ND + 1) * ND) >> 1 : 0;
+  const integer NA = NT - NR;
+
+  Femlib::synchronize();
+
+  ROOTONLY {
+    const char    routine[] = "Statistics::dump";
+    const integer verbose   = (integer) Femlib::value ("VERBOSE");
+    const integer chkpoint  = (integer) Femlib::value ("CHKPOINT");
+    char          dumpfl[StrMax], backup[StrMax], command[StrMax];
+
+    if (chkpoint) {
+      if (final) {
+	strcat (strcpy (dumpfl, name), ".avg");
+	output.open (dumpfl, ios::out);
+      } else {
+	strcat (strcpy (dumpfl, name), ".ave");
+	if (!initial) {
+	  strcat  (strcpy (backup, name), ".ave.bak");
+	  sprintf (command, "mv ./%s ./%s", dumpfl, backup);
+	  system  (command);
+	}
+	output.open (dumpfl, ios::out);
+      }
+    } else {
+      strcat (strcpy (dumpfl, name), ".avg");
+      if   (initial) output.open (dumpfl, ios::out);
+      else           output.open (dumpfl, ios::app);
+    }
+
+    if (!output) message (routine, "can't open dump file", ERROR);
+    if (verbose) message (routine, ": writing field dump", REMARK);
+  }
+
+  output << *this;
+
+  ROOTONLY output.close();
+}
+
+
+ofstream& operator << (ofstream&   strm,
+		       Statistics& src )
+// ---------------------------------------------------------------------------
+// Output Statistics class to file.  Like similar Domain routine.
+// ---------------------------------------------------------------------------
+{
+  integer           i;
+  const integer     N = src.avg.getSize();
+  vector<AuxField*> field (N);
+
+  for (i = 0; i < N; i++) field[i] = src.avg[i];
+
+  writeField (strm, src.name, src.navg, src.base -> time, field);
+
+  return strm;
+}
+
+
+ifstream& operator >> (ifstream&   strm,
+		       Statistics& tgt )
+// ---------------------------------------------------------------------------
+// Input Statistics class from file.  Like similar Domain routine.
+// ---------------------------------------------------------------------------
+{
+  const char routine[] = "strm>>Statistics";
+  integer    i, j, np, nz, nel, ntot, nfields;
+  integer    npchk,  nzchk, nelchk, swap = 0;
+  char       s[StrMax], f[StrMax], err[StrMax], fields[StrMax];
+
+  if (strm.getline(s, StrMax).eof()) return strm;
+  
+  strm.getline (s, StrMax) . getline (s, StrMax);
+
+  tgt.avg[0] -> describe (f);
+  istrstream (s, strlen (s)) >> np    >> np    >> nz    >> nel;
+  istrstream (f, strlen (f)) >> npchk >> npchk >> nzchk >> nelchk;
+  
+  if (np  != npchk ) message (routine, "element size mismatch",       ERROR);
+  if (nz  != nzchk ) message (routine, "number of z planes mismatch", ERROR);
+  if (nel != nelchk) message (routine, "number of elements mismatch", ERROR);
+  
+  ntot = np * np * nz * nel;
+  if (ntot != Geometry::nTot())
+    message (routine, "declared sizes mismatch", ERROR);
+
+  strm.getline (s,StrMax);
+  istrstream (s, strlen (s)) >> tgt.navg;
+    
+  strm.getline (s, StrMax) . getline (s, StrMax);
+  strm.getline (s, StrMax) . getline (s, StrMax) . getline (s, StrMax);
+    
+  nfields = 0;
+  while (isalpha (s[nfields])) {
+    fields[nfields] = s[nfields];
+    nfields++;
+  }
+  fields[nfields] = '\0';
+  if (nfields != tgt.avg.getSize()) {
+    sprintf (err, "strm: %1d fields, avg: %1d", nfields, tgt.avg.getSize());
+    message (routine, err, ERROR);
+  }
+  for (i = 0; i < nfields; i++) 
+    if (!strchr (fields, tgt.avg[i] -> name())) {
+      sprintf (err, "field %c not present in avg", fields[i]);
+      message (routine, err, ERROR);
+    }
+
+  strm.getline (s, StrMax);
+  Veclib::describeFormat (f);
+
+  if (!strstr (s, "binary"))
+    message (routine, "input field strm not in binary format", ERROR);
+  
+  if (!strstr (s, "endian"))
+    message (routine, "input field strm in unknown binary format", WARNING);
+  else {
+    swap = ((strstr (s, "big") && strstr (f, "little")) ||
+	    (strstr (f, "big") && strstr (s, "little")) );
+    ROOTONLY {
+      if (swap) cout << " (byte-swapping)";
+      cout.flush();
+    }
+  }
+    
+  for (j = 0; j < nfields; j++) {
+    for (i = 0; i < nfields; i++)
+      if (tgt.avg[i] -> name() == fields[j]) break;
+    strm >> *tgt.avg[i];
+    if (swap) tgt.avg[i] -> reverse();
+  }
+  
+  ROOTONLY if (strm.bad())
+    message (routine, "failed reading average file", ERROR);
+
+  return strm;
+}
+
+
+
