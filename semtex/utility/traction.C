04c4bbf472e5a918e5e135818efcadb5af50181b
hmb <>
2006-10-27 08:53:21 +0000

    update to traction.C - not tested yet, but compiles

--- semtex/utility/traction.C
+++ semtex/utility/traction.C
@@ -36,6 +36,8 @@ int main (int    argc,
   BCmgr*                     B;
   Domain*                    D;
   vector<Element*>           E;
+  int_t                      _nwall, _nline, _npad, DIM;
+  vector<real_t>             _work;
 
   Femlib::initialize (&argc, &argv);
 
@@ -47,10 +49,109 @@ int main (int    argc,
 
   preprocess (session, F, M, E, B, D);
 
+  DIM = Geometry::nDim();
+
   // -- Loop over all dumps in field file, compute and print traction.
 
   while (getDump (D, *file)) {
 
+    // -- Set up to compute wall shear stresses.    
+    
+    const int_t npr = Geometry::nProc();
+    const int_t np  = Geometry::nP();
+    const int_t nz  = Geometry::nZProc();
+
+    // -- Allocate storage area: 3 = 1 normal component + 2 tangential.
+    
+    _nwall = B -> nWall();
+    _nline = np * _nwall;
+    _npad  = 3  * _nline;
+
+    // -- Round up length for Fourier transform/exchange.
+
+    if   (npr > 1) _npad += 2 * npr - _npad % (2 * npr);
+    else           _npad += _npad % 2;
+
+    _work.resize (_npad * nz);
+
+    // --------------------------------------------------------------------
+
+    const int_t    nP  = Geometry::nP();
+    const int_t    nZ  = Geometry::nZ();
+    const int_t    nZP = Geometry::nZProc();
+    const int_t    nPR = Geometry::nProc();
+    const int_t    nPP = _npad / nPR;
+    int_t          i, j, k;
+    real_t*        plane;
+    vector<real_t> buffer (_nline);
+
+    // -- Load the local storage area.
+
+    Veclib::zero (_work.size(), &_work[0], 1);
+
+    if (DIM == 3 || D -> nField() == 4)
+      Field::traction (&_work[0], &_work[_nline], &_work[2*_nline], _nwall,
+		       _npad, D->u[3],D->u[0],D->u[1],D->u[2]);
+    else
+      Field::traction (&_work[0], &_work[_nline], &_work[2*_nline], _nwall,
+		       _npad, D->u[2],D->u[0],D->u[1]);
+
+    // -- Inverse Fourier transform (like Field::bTransform).
+
+    if (nPR == 1) {
+      if (nZ > 1)
+	if (nZ == 2)
+	  Veclib::copy (_npad, &_work[0], 1, &_work[_npad], 1);
+	else
+	  Femlib::DFTr (&_work[0], nZ, _npad, INVERSE);
+    } else {
+      Femlib::exchange (&_work[0], nZP, nP,  FORWARD);
+      Femlib::DFTr     (&_work[0], nZ,  nPP, INVERSE);
+      Femlib::exchange (&_work[0], nZP, nP,  INVERSE);
+    }
+
+    // -- Write to file.
+    
+    // -- Header: this will be a lot like a standard header.
+    //    Output normal and tangential tractions, 'n', 't', 's'.
+
+    const char *Hdr_Fmt[] = { 
+      "%-25s "                "Session\n",
+      "%-25s "                "Created\n",
+      "%-5d1    %-5d %-10d"   "Nr, Ns, Nz, Elements\n",
+      "%-25d "                "Step\n",
+      "%-25.6g "              "Time\n",
+      "%-25.6g "              "Time step\n",
+      "%-25.6g "              "Kinvis\n",
+      "%-25.6g "              "Beta\n",
+      "%-25s "                "Fields written\n",
+      "%-25s "                "Format\n" };
+      
+    char   s1[StrMax], s2[StrMax];
+    time_t tp (time (0));
+
+    strftime (s2, 25, "%a %b %d %H:%M:%S %Y", localtime (&tp));
+	
+    sprintf (s1, Hdr_Fmt[0], D->name);                  cout << s1;
+    sprintf (s1, Hdr_Fmt[1], s2);                       cout << s1;
+    sprintf (s1, Hdr_Fmt[2], nP, nZ, _nwall);           cout << s1;
+    sprintf (s1, Hdr_Fmt[3], D->step);                  cout << s1;
+    sprintf (s1, Hdr_Fmt[4], D->time);                  cout << s1;
+    sprintf (s1, Hdr_Fmt[5], Femlib::value ("D_T"));    cout << s1;
+    sprintf (s1, Hdr_Fmt[6], Femlib::value ("KINVIS")); cout << s1;
+    sprintf (s1, Hdr_Fmt[7], Femlib::value ("BETA"));   cout << s1;
+    sprintf (s1, Hdr_Fmt[8], "nts");                    cout << s1;
+    sprintf (s2, "binary "); Veclib::describeFormat  (s2 + strlen (s2));
+    sprintf (s1, Hdr_Fmt[9], s2);                       cout << s1;
+
+    // -- Data.
+  
+    for (j = 0; j < 3; j++)
+      for (i = 0; i < nZ; i++) {
+	plane = &_work[i*_npad + j*_nline];
+	cout.write (reinterpret_cast<char*>(plane),
+		    static_cast<int_t>(_nline * sizeof (real_t))); 
+      }
   }
   
   Femlib::finalize();
