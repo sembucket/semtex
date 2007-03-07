bbbdf431ff7a15dc9069e0e39f958cf40c40a971
hmb <>
2007-03-07 12:12:41 +0000

    started the changeover to GPL

--- femlib/polylib.c
+++ femlib/polylib.c
@@ -13,10 +13,29 @@
 /*
 LIBRARY ROUTINES FOR ORTHOGONAL POLYNOMIAL CALCULUS AND INTERPOLATION
 
-  Spencer Sherwin
+  Copyright (c) 1995 <--> $Date$, Spencer Sherwin
+
   Aeronautics, Imperial College London
   
   Based on codes by Einar Ronquist and Ron Henderson
+
+ * This file is part of Semtex (and also Nektar).
+ * 
+ * Semtex is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ * 
+ * Semtex is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Semtex (see the file COPYING); if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+
 
   Abbreviations
   - z    -   Set of collocation/quadrature points
