f5abea511436088922845bde0d453a6980880a89
hmb <>
2007-09-27 21:49:09 +0000

    moved to using gfortran for OS X

--- veclib/lapack.h
+++ veclib/lapack.h
@@ -16,86 +16,86 @@
 
 
 extern "C" {
-  void F77NAME(dgetrf) (const integer& m, const integer& n, double* a,
-			const integer& lda, integer* ipiv, integer& info);
-  void F77NAME(sgetrf) (const integer& m, const integer& n, float*  a,
-			const integer& lda, integer* ipiv, integer& info);
-  void F77NAME(dgetrs) (const char* tr, const integer& n, const integer& nrhs,
-			const double* a, const integer& lda, integer* ipiv,
-			double* b, const integer& ldb, integer& info);
-  void F77NAME(sgetrs) (const char* tr, const integer& n, const integer& nrhs,
-			const float*  a, const integer& lda, integer* ipiv,
-			float*  b, const integer& ldb, integer& info);
-  void F77NAME(dgecon) (const char* norm, const integer& n, const double* a,
+  void F77NAME(dgetrf) (const int_t& m, const int_t& n, double* a,
+			const int_t& lda, int_t* ipiv, int_t& info);
+  void F77NAME(sgetrf) (const int_t& m, const int_t& n, float*  a,
+			const int_t& lda, int_t* ipiv, int_t& info);
+  void F77NAME(dgetrs) (const char* tr, const int_t& n, const int_t& nrhs,
+			const double* a, const int_t& lda, int_t* ipiv,
+			double* b, const int_t& ldb, int_t& info);
+  void F77NAME(sgetrs) (const char* tr, const int_t& n, const int_t& nrhs,
+			const float*  a, const int_t& lda, int_t* ipiv,
+			float*  b, const int_t& ldb, int_t& info);
+  void F77NAME(dgecon) (const char* norm, const int_t& n, const double* a,
 			const double& anorm, double& rcond, double* rwrk,
-			integer* iwork, integer& info);
-  void F77NAME(sgecon) (const char* norm, const integer& n, const float*  a,
+			int_t* iwork, int_t& info);
+  void F77NAME(sgecon) (const char* norm, const int_t& n, const float*  a,
 			const float&  anorm, float&  rcond, float*  rwrk,
-			integer* iwork, integer& info);
-  void F77NAME(dgetri) (const integer& n, double* a, const integer& lda,
-			integer* ipiv,  
-			double* work, const integer& lwork, integer& info);
-  void F77NAME(sgetri) (const integer& n, float*  a, const integer& lda,
-			integer* ipiv,
-			float*  work, const integer& lwork, integer& info);
-  void F77NAME(dpbtrf) (const char* uplo, const integer& n, const integer& kd, 
-			double* ab, const integer& ldab, integer& info);
-  void F77NAME(spbtrf) (const char* uplo, const integer& n, const integer& kd,
-			float*  ab, const integer& ldab, integer& info);
-  void F77NAME(dpbcon) (const char* uplo, const integer& n, const integer& kd,
-			const double* ab, const integer& ldab,
+			int_t* iwork, int_t& info);
+  void F77NAME(dgetri) (const int_t& n, double* a, const int_t& lda,
+			int_t* ipiv,  
+			double* work, const int_t& lwork, int_t& info);
+  void F77NAME(sgetri) (const int_t& n, float*  a, const int_t& lda,
+			int_t* ipiv,
+			float*  work, const int_t& lwork, int_t& info);
+  void F77NAME(dpbtrf) (const char* uplo, const int_t& n, const int_t& kd, 
+			double* ab, const int_t& ldab, int_t& info);
+  void F77NAME(spbtrf) (const char* uplo, const int_t& n, const int_t& kd,
+			float*  ab, const int_t& ldab, int_t& info);
+  void F77NAME(dpbcon) (const char* uplo, const int_t& n, const int_t& kd,
+			const double* ab, const int_t& ldab,
 			const double& anorm, double& rcond,
-			double* rwrk, integer* iwork, integer& info);
-  void F77NAME(spbcon) (const char* uplo, const integer& n, const integer& kd,
-			const float*  ab, const integer& ldab,
+			double* rwrk, int_t* iwork, int_t& info);
+  void F77NAME(spbcon) (const char* uplo, const int_t& n, const int_t& kd,
+			const float*  ab, const int_t& ldab,
 			const float&  anorm, float& rcond,
-			float* rwrk, integer* iwork, integer& info);
-  void F77NAME(dpbtrs) (const char* uplo, const integer& n, 
-			const integer& kd, const integer& nrhs,
-			const double* ab, const integer& ldab,
-			double* b, const integer& ldb, integer& info);
-  void F77NAME(spbtrs) (const char* uplo, const integer& n,
-			const integer& kd, const integer& nrhs,
-			const float*  ab, const integer& ldab,
-			float*  b, const integer& ldb, integer& info);
-  void F77NAME(dpptrf) (const char* uplo, const integer& n,
-			double* ap, integer& info);
-  void F77NAME(spptrf) (const char* uplo, const integer& n, 
-			float*  ap, integer& info);
-  void F77NAME(dppcon) (const char* uplo, const integer& n, const double* ap,
+			float* rwrk, int_t* iwork, int_t& info);
+  void F77NAME(dpbtrs) (const char* uplo, const int_t& n, 
+			const int_t& kd, const int_t& nrhs,
+			const double* ab, const int_t& ldab,
+			double* b, const int_t& ldb, int_t& info);
+  void F77NAME(spbtrs) (const char* uplo, const int_t& n,
+			const int_t& kd, const int_t& nrhs,
+			const float*  ab, const int_t& ldab,
+			float*  b, const int_t& ldb, int_t& info);
+  void F77NAME(dpptrf) (const char* uplo, const int_t& n,
+			double* ap, int_t& info);
+  void F77NAME(spptrf) (const char* uplo, const int_t& n, 
+			float*  ap, int_t& info);
+  void F77NAME(dppcon) (const char* uplo, const int_t& n, const double* ap,
 			const double& anorm, double& rcond, double* rwrk,
-			integer* iwrk, integer& info);
-  void F77NAME(sppcon) (const char* uplo, const integer& n, const float*  ap,
+			int_t* iwrk, int_t& info);
+  void F77NAME(sppcon) (const char* uplo, const int_t& n, const float*  ap,
 			const float&  anorm, float&  rcond, float*  rwrk,
-			integer* iwrk, integer& info);
-  void F77NAME(dpptrs) (const char* uplo, const integer& n,
-			const integer& nrhs, const double* ap,
-			double* b, const integer& ldb, integer& info);
-  void F77NAME(spptrs) (const char* uplo, const integer& n,
-			const integer& nrhs, const float*  ap, float*  b,
-			const integer& ldb, integer& info);
-  void F77NAME(dgesv)  (const integer& n, const integer& nrhs, double* a,
-			const integer& lda, integer* ipiv, double* b,
-			const integer& ldb, integer& info);
-  void F77NAME(sgesv)  (const integer& n, const integer& nrhs, float* a,
-			const integer& lda, integer* ipiv, float* b, 
-			const integer& ldb, integer& info);
-  void F77NAME(dgeev)  (const char* uplo, const char* lrev, const integer& n,
-			double* a, const integer& lda, double* wr, double* wi,
-			double* rev,  const integer& ldr,
-			double* lev,  const integer& ldv,
-			double* work, const integer& lwork, integer& info);
-  void F77NAME(sgeev)  (const char* uplo, const char* lrev, const integer& n,
-			float*  a, const integer& lda, float*  wr, float*  wi,
-			float*  rev,  const integer& ldr,
-			float*  lev,  const integer& ldv,
-			float*  work, const integer& lwork, integer& info);
-  void F77NAME(dspev)  (const char* jobz, const char* uplo, const integer& n,
-			double* ap, double* w, double* z, const integer& ldz,
-			double* work, integer& info);
-  void F77NAME(sspev)  (const char* jobz, const char* uplo, const integer& n,
-			float* ap, float* w, float* z, const integer& ldz,
-			float* work, integer& info);
+			int_t* iwrk, int_t& info);
+  void F77NAME(dpptrs) (const char* uplo, const int_t& n,
+			const int_t& nrhs, const double* ap,
+			double* b, const int_t& ldb, int_t& info);
+  void F77NAME(spptrs) (const char* uplo, const int_t& n,
+			const int_t& nrhs, const float*  ap, float*  b,
+			const int_t& ldb, int_t& info);
+  void F77NAME(dgesv)  (const int_t& n, const int_t& nrhs, double* a,
+			const int_t& lda, int_t* ipiv, double* b,
+			const int_t& ldb, int_t& info);
+  void F77NAME(sgesv)  (const int_t& n, const int_t& nrhs, float* a,
+			const int_t& lda, int_t* ipiv, float* b, 
+			const int_t& ldb, int_t& info);
+  void F77NAME(dgeev)  (const char* uplo, const char* lrev, const int_t& n,
+			double* a, const int_t& lda, double* wr, double* wi,
+			double* rev,  const int_t& ldr,
+			double* lev,  const int_t& ldv,
+			double* work, const int_t& lwork, int_t& info);
+  void F77NAME(sgeev)  (const char* uplo, const char* lrev, const int_t& n,
+			float*  a, const int_t& lda, float*  wr, float*  wi,
+			float*  rev,  const int_t& ldr,
+			float*  lev,  const int_t& ldv,
+			float*  work, const int_t& lwork, int_t& info);
+  void F77NAME(dspev)  (const char* jobz, const char* uplo, const int_t& n,
+			double* ap, double* w, double* z, const int_t& ldz,
+			double* work, int_t& info);
+  void F77NAME(sspev)  (const char* jobz, const char* uplo, const int_t& n,
+			float* ap, float* w, float* z, const int_t& ldz,
+			float* work, int_t& info);
 }
 
 
@@ -104,180 +104,180 @@ public:
 
   // -- See LAPACK Users' Guide, \S\S 5.3.2 & 5.3.3 for 'U' storage schemes:
 
-  static inline integer band_addr (integer i, integer j, integer bw)
+  static inline int_t band_addr (int_t i, int_t j, int_t bw)
   { return (j+1)*bw-j+i-1; }
-  static inline integer pack_addr (integer i, integer j)
+  static inline int_t pack_addr (int_t i, int_t j)
   { return  (((j+1)*j)>>1)+i; }
 
   // -- L-U factor a general real matrix.
 
-  static void getrf (const integer& m, const integer& n, double *a,
-		     const integer& lda, integer *ipiv, integer& info) {
+  static void getrf (const int_t& m, const int_t& n, double *a,
+		     const int_t& lda, int_t *ipiv, int_t& info) {
     F77NAME(dgetrf) (m,n,a,lda,ipiv,info);
   }
-  static void getrf (const integer& m, const integer& n, float  *a,
-		     const integer& lda, integer *ipiv, integer& info) {
+  static void getrf (const int_t& m, const int_t& n, float  *a,
+		     const int_t& lda, int_t *ipiv, int_t& info) {
     F77NAME(sgetrf) (m,n,a,lda,ipiv,info);
   }
 
   // -- Solve a system of equations from L-U factorization.
 
-  static void getrs (const char* trans, const integer& n, const integer& nrhs,
-		     const double* a, const integer& lda, integer* ipiv,
-		     double* b, const integer& ldb, integer& info) {
+  static void getrs (const char* trans, const int_t& n, const int_t& nrhs,
+		     const double* a, const int_t& lda, int_t* ipiv,
+		     double* b, const int_t& ldb, int_t& info) {
     F77NAME(dgetrs) (trans,n,nrhs,a,lda,ipiv,b,ldb,info);
   }
-  static void getrs (const char *trans, const integer& n, const integer& nrhs,
-		     const float*  a, const integer& lda, integer* ipiv,
-		     float*  b, const integer& ldb, integer& info) {
+  static void getrs (const char *trans, const int_t& n, const int_t& nrhs,
+		     const float*  a, const int_t& lda, int_t* ipiv,
+		     float*  b, const int_t& ldb, int_t& info) {
     F77NAME(sgetrs) (trans,n,nrhs,a,lda,ipiv,b,ldb,info);
   }
   // -- Estimate condition number of general matrix from factorization.
 
-  static void gecon (const char* norm, const integer& n, const double* a,
+  static void gecon (const char* norm, const int_t& n, const double* a,
 		     const double& anorm, double& rcond, double* rwrk,
-		     integer* iwork, integer& info) {
+		     int_t* iwork, int_t& info) {
     F77NAME(dgecon) (norm,n,a,anorm,rcond,rwrk,iwork,info);
   }
-  static void gecon (const char* norm, const integer& n, const float*  a,
+  static void gecon (const char* norm, const int_t& n, const float*  a,
 		     const float&  anorm, float&  rcond, float*  rwrk,
-		     integer* iwork, integer& info) {
+		     int_t* iwork, int_t& info) {
     F77NAME(sgecon) (norm,n,a,anorm,rcond,rwrk,iwork,info);
   }
 
   // -- Invert a general real matrix from L-U factors.
 
-  static void getri (const integer& n, double *a, const integer& lda,
-		     integer *ipiv, double *work, const integer& lwork,
-		     integer& info) {
+  static void getri (const int_t& n, double *a, const int_t& lda,
+		     int_t *ipiv, double *work, const int_t& lwork,
+		     int_t& info) {
     F77NAME(dgetri) (n,a,lda,ipiv,work,lwork,info);
   }
-  static void getri (const integer& n, float  *a, const integer& lda,
-		     integer *ipiv, float  *work, const integer& lwork, 
-		     integer& info) {
+  static void getri (const int_t& n, float  *a, const int_t& lda,
+		     int_t *ipiv, float  *work, const int_t& lwork, 
+		     int_t& info) {
     F77NAME(sgetri) (n,a,lda,ipiv,work,lwork,info);
   }
 
   // -- Cholesky factorize a real positive-definite band-symmetric matrix.
 
-  static void pbtrf (const char *uplo, const integer& n, const integer& kd, 
-		     double *ab, const integer& ldab, integer& info) {
+  static void pbtrf (const char *uplo, const int_t& n, const int_t& kd, 
+		     double *ab, const int_t& ldab, int_t& info) {
     F77NAME(dpbtrf) (uplo,n,kd,ab,ldab,info);
   }
-  static void pbtrf (const char *uplo, const integer& n, const integer& kd,
-		     float  *ab, const integer& ldab, integer& info) {
+  static void pbtrf (const char *uplo, const int_t& n, const int_t& kd,
+		     float  *ab, const int_t& ldab, int_t& info) {
     F77NAME(spbtrf) (uplo,n,kd,ab,ldab,info);
   }
 
   // -- Estimate condition number of a real P-D B-S matrix from factorization.
 
-  static void pbcon (const char* uplo, const integer& n, const integer& kd,
-		     const double* ab, const integer& ldab,
+  static void pbcon (const char* uplo, const int_t& n, const int_t& kd,
+		     const double* ab, const int_t& ldab,
 		     const double& anorm, double& rcond,
-		     double* rwrk, integer* iwork, integer& info) {
+		     double* rwrk, int_t* iwork, int_t& info) {
     F77NAME(dpbcon) (uplo,n,kd,ab,ldab,anorm,rcond,rwrk,iwork,info);
   }
-  static void pbcon (const char* uplo, const integer& n, const integer& kd,
-		     const float*  ab, const integer& ldab,
+  static void pbcon (const char* uplo, const int_t& n, const int_t& kd,
+		     const float*  ab, const int_t& ldab,
 		     const float&  anorm, float&  rcond,
-		     float*  rwrk, integer* iwork, integer& info) {
+		     float*  rwrk, int_t* iwork, int_t& info) {
     F77NAME(spbcon) (uplo,n,kd,ab,ldab,anorm,rcond,rwrk,iwork,info);
   }
 
   // -- Solve a real, P-D B-S matrix problem using Cholesky factorization.
 
-  static void pbtrs (const char *uplo, const integer& n,
-		     const integer& kd, const integer& nrhs,
-		     const double *ab, const integer& ldab,
-		     double *b, const integer& ldb, integer& info) {
+  static void pbtrs (const char *uplo, const int_t& n,
+		     const int_t& kd, const int_t& nrhs,
+		     const double *ab, const int_t& ldab,
+		     double *b, const int_t& ldb, int_t& info) {
     F77NAME(dpbtrs) (uplo,n,kd,nrhs,ab,ldab,b,ldb,info);
   }
-  static void pbtrs (const char *uplo, const integer& n,
-		     const integer& kd, const integer& nrhs,
-		     const float  *ab, const integer& ldab,
-		     float  *b, const integer& ldb, integer& info) {
+  static void pbtrs (const char *uplo, const int_t& n,
+		     const int_t& kd, const int_t& nrhs,
+		     const float  *ab, const int_t& ldab,
+		     float  *b, const int_t& ldb, int_t& info) {
     F77NAME(spbtrs) (uplo,n,kd,nrhs,ab,ldab,b,ldb,info);
   }
 
   // -- Cholesky factor a real P-D packed-symmetric matrix.
 
-  static void pptrf (const char *uplo, const integer& n,
-		     double *ap, integer& info) {
+  static void pptrf (const char *uplo, const int_t& n,
+		     double *ap, int_t& info) {
     F77NAME(dpptrf) (uplo,n,ap,info);
   }
-  static void pptrf (const char *uplo, const integer& n,
-		     float  *ap, integer& info) {
+  static void pptrf (const char *uplo, const int_t& n,
+		     float  *ap, int_t& info) {
     F77NAME(spptrf) (uplo,n,ap,info);
   }  
 
   // -- Estimate condition number of a P-D P-S matrix from factorization.
 
-  static void ppcon (const char* uplo, const integer& n, const double* ap,
+  static void ppcon (const char* uplo, const int_t& n, const double* ap,
 		     const double& anorm, double& rcond, double* rwrk,
-		     integer* iwrk, integer& info) {
+		     int_t* iwrk, int_t& info) {
     F77NAME(dppcon) (uplo,n,ap,anorm,rcond,rwrk,iwrk,info);
   }
-  static void ppcon (const char* uplo, const integer& n, const float*  ap,
+  static void ppcon (const char* uplo, const int_t& n, const float*  ap,
 		     const float&  anorm, float&  rcond, float*  rwrk,
-		     integer* iwrk, integer& info) {
+		     int_t* iwrk, int_t& info) {
     F77NAME(sppcon) (uplo,n,ap,anorm,rcond,rwrk,iwrk,info);
   }
 
   // -- Solve a real P-D P-S matrix problem using Cholesky factorization.
 
-  static void pptrs (const char *uplo, const integer& n, const integer& nrhs,
-		     const double *ap, double *b, const integer& ldb,
-		     integer& info) {
+  static void pptrs (const char *uplo, const int_t& n, const int_t& nrhs,
+		     const double *ap, double *b, const int_t& ldb,
+		     int_t& info) {
     F77NAME(dpptrs) (uplo,n,nrhs,ap,b,ldb,info);
   }
-  static void pptrs (const char *uplo, const integer& n, const integer& nrhs,
-		     const float  *ap, float  *b, const integer& ldb,
-		     integer& info) {
+  static void pptrs (const char *uplo, const int_t& n, const int_t& nrhs,
+		     const float  *ap, float  *b, const int_t& ldb,
+		     int_t& info) {
     F77NAME(spptrs) (uplo,n,nrhs,ap,b,ldb,info);
   }
 
   // -- Solve a general system of real equations.
 
-  static void gesv (const integer& n, const integer& nrhs,
-		    double* a, const integer& lda, integer* ipiv,
-		    double* b, const integer& ldb, integer& info) {
+  static void gesv (const int_t& n, const int_t& nrhs,
+		    double* a, const int_t& lda, int_t* ipiv,
+		    double* b, const int_t& ldb, int_t& info) {
     F77NAME(dgesv) (n,nrhs,a,lda,ipiv,b,ldb,info);
   }
-  static void gesv (const integer& n, const integer& nrhs,
-		    float*  a, const integer& lda, integer* ipiv,
-		    float*  b, const integer& ldb, integer& info) {
+  static void gesv (const int_t& n, const int_t& nrhs,
+		    float*  a, const int_t& lda, int_t* ipiv,
+		    float*  b, const int_t& ldb, int_t& info) {
     F77NAME(sgesv) (n,nrhs,a,lda,ipiv,b,ldb,info);
   }
 
   // -- Solve general real matrix eigenproblem.
 
-  static void geev (const char* uplo, const char* lrev, const integer& n,
-		    double* a, const integer& lda, double* wr, double* wi,
-		    double* rev,  const integer& ldr,
-		    double* lev,  const integer& ldv,
-		    double* work, const integer& lwork, integer& info) {
+  static void geev (const char* uplo, const char* lrev, const int_t& n,
+		    double* a, const int_t& lda, double* wr, double* wi,
+		    double* rev,  const int_t& ldr,
+		    double* lev,  const int_t& ldv,
+		    double* work, const int_t& lwork, int_t& info) {
     F77NAME(dgeev) (uplo, lrev, n, a, lda, wr, wi, rev,
 			 ldr, lev, ldv, work, lwork, info);
   }
-  static void geev (const char* uplo, const char* lrev, const integer& n,
-		    float*  a, const integer& lda, float*  wr, float*  wi,
-		    float*  rev,  const integer& ldr,
-		    float*  lev,  const integer& ldv,
-		    float*  work, const integer& lwork, integer& info) {
+  static void geev (const char* uplo, const char* lrev, const int_t& n,
+		    float*  a, const int_t& lda, float*  wr, float*  wi,
+		    float*  rev,  const int_t& ldr,
+		    float*  lev,  const int_t& ldv,
+		    float*  work, const int_t& lwork, int_t& info) {
     F77NAME(sgeev) (uplo, lrev, n, a, lda, wr, wi, rev,
 			 ldr, lev, ldv, work, lwork, info);
   }
 
   // -- Solve packed-symmetric real matrix eigenproblem.
 
-  static void spev (const char* jobz, const char* uplo, const integer& n,
-		    double* ap, double* w, double* z, const integer& ldz,
-		    double* work, integer& info) {
+  static void spev (const char* jobz, const char* uplo, const int_t& n,
+		    double* ap, double* w, double* z, const int_t& ldz,
+		    double* work, int_t& info) {
     F77NAME(dspev) (jobz, uplo, n, ap, w, z, ldz, work, info);
   }
-  static void spev (const char* jobz, const char* uplo, const integer& n,
-		    float* ap, float* w, float* z, const integer& ldz,
-		    float* work, integer& info) {
+  static void spev (const char* jobz, const char* uplo, const int_t& n,
+		    float* ap, float* w, float* z, const int_t& ldz,
+		    float* work, int_t& info) {
     F77NAME(sspev) (jobz, uplo, n, ap, w, z, ldz, work, info);
   }
 
