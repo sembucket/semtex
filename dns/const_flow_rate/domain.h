1e0a174ea40a6ed5aaa26b70a29fc3576bb06a27
semuser <semuser@138_80_98_102.cdu.edu.au>
2019-08-09 16:57:04 +1000

    adding a revised version of the integrate routine for a prescribed constant mass flux (as opposed to a pressure gradient driven flow)

---
+++ dns/const_flow_rate/domain.h
@@ -0,0 +1,42 @@
+#ifndef DOMAIN_H
+#define DOMAIN_H
+
+class Domain
+// ===========================================================================
+// Physical domain storage class.
+//
+// Since users are expected to program with entities kept at the
+// Domain level, all internal storage is exposed to view (like a C
+// struct).
+//
+// Domain fields are written/read from/to file untransformed (in
+// physical space).
+// ===========================================================================
+{
+friend istream& operator >> (istream&, Domain&);
+friend ostream& operator << (ostream&, Domain&);
+public:
+  Domain (FEML*, vector<Element*>&, BCmgr*);
+
+  char*                name;  // Session name.
+  char*                field; // Lower-case single character field names.
+  int_t                step;  // Runtime step number.
+  real_t               time;  // Simulation time.
+  vector<Element*>&    elmt;  // Shared for equal-order interpolations.
+  vector<real_t*>      udat;  // Data storage area for solution fields.
+  vector<Field*>       u   ;  // Solution fields: velocities, scalar, pressure.
+  vector<BoundarySys*> b   ;  // Field boundary systems.
+  vector<AuxField*>    grn ;  // Stokes problem with unit forcing (for the const. mass flux formulation)
+
+  int_t nField     () const { return u.size(); }
+  int_t nAdvect    () const { return u.size() - 1; } // No. of advected terms.
+  int_t nVelCmpt   () const { return                 // "" velocity components.
+                              (hasScalar()) ? u.size() - 2 : u.size() - 1; }
+  bool  hasScalar  () const { return strchr (field, 'c'); }
+  void  report     ();
+  void  restart    ();
+  void  dump       ();
+  void  transform  (const int_t);
+};
+
+#endif
