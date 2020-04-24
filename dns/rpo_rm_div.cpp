2c47610e204d043916633f2eeb505a84bfcaa64b
semuser <semuser@aquila.local>
2020-04-24 10:54:02 +1000

    adding utility to remove the divergence from a vector field (requires a scalar field 'c' with homogeneous neumann bcs)

---
+++ dns/rpo_rm_div.cpp
@@ -0,0 +1,286 @@
+//////////////////////////////////////////////////////////////////////////////
+// drive.C: control spectral element DNS for incompressible flows.
+//
+// Copyright (c) 1994 <--> $Date$, Hugh Blackburn
+//
+// USAGE:
+// -----
+// dns [options] session
+//   options:
+//   -h       ... print usage prompt
+//   -i       ... use iterative solver for viscous [and pressure] steps
+//   -t[t]    ... select time-varying BCs for mode 0 [or all modes]
+//   -v[v...] ... increase verbosity level
+//   -chk     ... turn off checkpoint field dumps [default: selected]
+//   -S|C|N   ... regular skew-symm || convective || Stokes advection
+//
+// AUTHOR:
+// ------
+// Hugh Blackburn
+// Department of Mechanical & Aerospace Engineering
+// Monash University
+// Vic 3800
+// Australia
+// hugh.blackburn@monash.edu
+//
+// --
+// This file is part of Semtex.
+//
+// Semtex is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the
+// Free Software Foundation; either version 2 of the License, or (at your
+// option) any later version.
+//
+// Semtex is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Semtex (see the file COPYING); if not, write to the Free
+// Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+// 02110-1301 USA.
+//////////////////////////////////////////////////////////////////////////////
+
+static char RCS[] = "$Id$";
+
+#include <dns.h>
+
+#include <petsc.h>
+
+static char prog[] = "rpo";
+static void getargs    (int, char**, bool&, char*&);
+static void preprocess (const char*, FEML*&, Mesh*&, vector<Element*>&,
+			BCmgr*&, Domain*&, FieldForce*&);
+
+void _RemoveDivergence(Domain* D) {
+  int                    np      = Geometry::nP();
+  int                    np2     = Geometry::nTotElmt();
+  const int_t            nmodes  = Geometry::nModeProc();
+  const int_t            base    = Geometry::baseMode(); 
+  const real_t           beta    = Femlib::value("BETA");
+  ModalMatrixSys*        mss     = new ModalMatrixSys(0, beta, base, nmodes, D->elmt, D->b[3], DIRECT);
+  AuxField*              div;
+  AuxField*              wrk;
+
+  div = new AuxField(new real_t[Geometry::nTotal()], Geometry::nZProc(), D->elmt);
+  wrk = new AuxField(new real_t[Geometry::nTotal()], Geometry::nZProc(), D->elmt);
+
+  // manually compute the divergence and put this in the pressure field (for testing)
+  *D->u[4] = 0.0;
+  for(int ii = 0; ii < 3; ii++) {
+    *div = *D->u[ii];
+    div->gradient(ii);
+    if(ii == 2) div->divY();
+    //D->u[0]->smooth(div);
+    *D->u[4] += *div;
+  }
+  *div = *D->u[1];
+  div->divY();
+  *D->u[4] += *div;
+  D->u[4]->smooth(D->u[4]);
+  D->u[4]->zeroNyquist();
+
+  // compute the divergence (scaled by the radius)
+  *div = 0.0;
+  for(int ii = 0; ii < 3; ii++) {
+    *wrk = *D->u[ii];
+    if(ii < 2) wrk->mulY();
+    wrk->gradient(ii);
+    //D->u[0]->smooth(wrk);
+    *div += *wrk;
+  }
+
+  *D->u[3] = 0.0;
+  D->u[3]->solve(div, mss);
+  D->u[3]->zeroNyquist();
+
+  // remove the divergence from the initial velocity field
+  for(int ii = 0; ii < 3; ii++) {
+    *div = *D->u[3];
+    div->gradient(ii);
+    if(ii == 2) div->divY();
+    //D->u[0]->smooth(div);
+    *D->u[ii] -= *div;
+  }
+
+  // now re-compute the divergence of the updated velocity, and put this in the scalar field
+  *D->u[3] = 0.0;
+  for(int ii = 0; ii < 3; ii++) {
+    *div = *D->u[ii];
+    div->gradient(ii);
+    if(ii == 2) div->divY();
+    //D->u[0]->smooth(div);
+    *D->u[3] += *div;
+  }
+  *div = *D->u[1];
+  div->divY();
+  *D->u[3] += *div;
+  D->u[3]->smooth(D->u[3]);
+  D->u[3]->zeroNyquist();
+}
+
+int main (int argc, char** argv) {
+#ifdef _GNU_SOURCE
+  feenableexcept (FE_OVERFLOW);    // -- Force SIG8 crash on FP overflow.
+#endif
+  char*            session;
+  bool             freeze = false;
+  vector<Element*> elmt;
+  FEML*            file;
+  Mesh*            mesh;
+  BCmgr*           bman;
+  Domain*          domain;
+  DNSAnalyser*     analyst;
+  FieldForce*      FF;
+  static char      help[] = "petsc";
+
+  PetscInitialize(&argc, &argv, (char*)0, help);
+
+  Femlib::initialize (&argc, &argv);
+  getargs (argc, argv, freeze, session);
+  preprocess (session, file, mesh, elmt, bman, domain, FF);
+  domain->restart();
+
+  analyst = new DNSAnalyser (domain, bman, file);
+  
+  _RemoveDivergence(domain);
+  domain->dump();
+
+  if(!Geometry::procID()) cout << "...done.\n";
+
+  PetscFinalize();
+
+  return EXIT_SUCCESS;
+}
+
+static void getargs (int    argc   ,
+		     char** argv   ,
+		     bool&  freeze ,
+		     char*& session)
+// ---------------------------------------------------------------------------
+// Install default parameters and options, parse command-line for optional
+// arguments.  Last argument is name of a session file, not dealt with here.
+// ---------------------------------------------------------------------------
+{
+  char       buf[StrMax];
+  const char routine[] = "getargs";
+  const char usage[]   = "Usage: %s [options] session-file\n"
+    "  [options]:\n"
+    "  -h       ... print this message\n"
+    "  -f       ... freeze velocity field (scalar advection/diffusion only)\n"
+    "  -i       ... use iterative solver for viscous steps\n"
+    "  -v[v...] ... increase verbosity level\n"
+    "  -chk     ... turn off checkpoint field dumps [default: selected]\n"
+    "  -S|C|N   ... regular skew-symm || convective || Stokes advection\n";
+
+  Femlib::ivalue ("ADVECTION", 1); // -- Default is alternating skew symmetric.
+
+  while (--argc  && **++argv == '-')
+    switch (*++argv[0]) {
+    case 'h':
+      sprintf (buf, usage, prog);
+      cout << buf;
+      exit (EXIT_SUCCESS);
+      break;
+    case 'S': Femlib::ivalue ("ADVECTION", 0); break;
+    case 'C': Femlib::ivalue ("ADVECTION", 2); break;
+    case 'N': Femlib::ivalue ("ADVECTION", 3); break;
+    case 'f':
+      freeze = true;
+      break;
+    case 'i':
+      do			// -- Only allowing ITERATIVE=1 (Viscous).
+	Femlib::ivalue ("ITERATIVE", 1);
+      while (*++argv[0] == 'i');
+      break;
+    case 'v':
+      do
+	Femlib::ivalue ("VERBOSE",   Femlib::ivalue ("VERBOSE")   + 1);
+      while (*++argv[0] == 'v');
+      break;
+    case 'c':
+      if (strstr ("chk", *argv))     Femlib::ivalue ("CHKPOINT",    0);
+      else { fprintf (stdout, usage, prog); exit (EXIT_FAILURE); }
+      break;
+    default:
+      sprintf (buf, usage, prog);
+      cout << buf;
+      exit (EXIT_FAILURE);
+      break;
+    }
+
+  //if   (argc != 1) message (routine, "no session definition file", ERROR);
+  //else             session = *argv;
+  session = *argv;
+
+  Femlib::value ("DTBDX", 0.0);
+}
+
+static void preprocess (const char*       session,
+			FEML*&            file   ,
+			Mesh*&            mesh   ,
+			vector<Element*>& elmt   ,
+			BCmgr*&           bman   ,
+			Domain*&          domain ,
+			FieldForce*&      FF     )
+// ---------------------------------------------------------------------------
+// Create objects needed for execution, given the session file name.
+// They are listed above in order of creation.
+// ---------------------------------------------------------------------------
+{
+
+  const char routine[] = "preprocess";
+  const int_t        verbose = Femlib::ivalue ("VERBOSE");
+  Geometry::CoordSys space;
+  int_t              i, np, nz, nel, procid, seed;
+
+  // -- Initialise problem and set up mesh geometry.
+  VERBOSE cout << "Building mesh ..." << endl;
+  file = new FEML (session);
+  mesh = new Mesh (file);
+  VERBOSE cout << "done" << endl;
+
+  // -- Set up global geometry variables.
+  VERBOSE cout << "Setting geometry ... ";
+  nel   =  mesh -> nEl();
+  np    =  Femlib::ivalue ("N_P");
+  nz    =  Femlib::ivalue ("N_Z");
+  space = (Femlib::ivalue ("CYLINDRICAL")) ?
+    Geometry::Cylindrical : Geometry::Cartesian;
+
+  Geometry::set (np, nz, nel, space);
+  VERBOSE cout << "done" << endl;
+
+  // -- If token RANSEED > 0 then initialize the random number
+  //    generator based on wall clock time and process ID (i.e. a "truly"
+  //    pseudo-random number).  NB: it is important to have done this
+  //    before any other possible call to random number routines.
+
+  if (Femlib::ivalue("RANSEED") > 0) {
+    procid = Geometry::procID();
+    seed   = -abs((procid + 1) * (char) time(NULL));
+  } else seed = -1;
+  Veclib::ranInit (seed);
+
+  // -- Build all the elements.
+  VERBOSE cout << "Building elements ... ";
+  elmt.resize (nel);
+  for (i = 0; i < nel; i++) elmt[i] = new Element (i, np, mesh);
+  VERBOSE cout << "done" << endl;
+
+  // -- Build all the boundary condition applicators.
+  VERBOSE cout << "Building boundary condition manager ..." << endl;
+  bman = new BCmgr (file, elmt);
+  VERBOSE cout << "done" << endl;
+
+  // -- Build the solution domain.
+  VERBOSE cout << "Building domain ..." << endl;
+  domain = new Domain (file, elmt, bman);
+  VERBOSE cout << "done" << endl;
+
+  // -- Build field force.
+  VERBOSE cout << "Building field force ..." << endl;
+  FF = new FieldForce (domain, file);
+  VERBOSE cout << "done" << endl;
+}
