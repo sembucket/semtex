99bc9ae506022703448285bda0a095bc6a644f2b
AlwinW <16846521+AlwinW@users.noreply.github.com>
2020-04-25 23:51:32 +1000

    Additional fixes to the workflow

--- .github/workflows/sync-upstream.sh
+++ .github/workflows/sync-upstream.sh
@@ -5,11 +5,6 @@ UPSTREAM_REMOTE=${UPSTREAM_REMOTE:-"upstream_remote"}
 UPSTREAM_REMOTE_NAME=${UPSTREAM_REMOTE_NAME:-"upstream"}
 UPSTREAM_REMOTE_URL=${UPSTREAM_REMOTE_URL:-"https://gitlab.erc.monash.edu.au/sembucket/semtex.git"}
 
-if [ -z "${INPUT_GITHUB_TOKEN}" ]; then
-    echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".' 
-    # exit 1
-fi
-
 echo "Pulling $UPSTREAM_REMOTE_URL"
 git remote add "$UPSTREAM_REMOTE" "$UPSTREAM_REMOTE_URL"
 git fetch "$UPSTREAM_REMOTE"
@@ -17,5 +12,5 @@ git fetch "$UPSTREAM_REMOTE"
 for branch in $(git ls-remote $UPSTREAM_REMOTE | grep -oP '(?<=refs/heads/).*'); do
     echo "Fetching branch '$branch' and setting upstream to '$MIRROR_REPO/$UPSTREAM_REMOTE_NAME/$branch'"
     git fetch "$UPSTREAM_REMOTE" "$branch:$UPSTREAM_REMOTE_NAME/$branch"
-    git push --dry-run --set-upstream "$MIRROR_REPO" "$UPSTREAM_REMOTE_NAME/$branch:$UPSTREAM_REMOTE_NAME/$branch"
+    git push --set-upstream "$MIRROR_REPO" "$UPSTREAM_REMOTE_NAME/$branch:$UPSTREAM_REMOTE_NAME/$branch"
 done
